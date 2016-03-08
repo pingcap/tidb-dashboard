a559c784ebbf0551aedf8bdb99511d927c0f05be
siddontang <siddontang@gmail.com>
2016-03-08 09:46:37 +0800

    protopb: add proto.

---
+++ protopb/errors.go
@@ -0,0 +1,25 @@
+package protopb
+
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+)
+
+// NewError returns a Response with go error.
+func NewError(err error) *Response {
+	r := &Response{
+		Header: &ResponseHeader{},
+	}
+
+	r.Header.Error = &Error{
+		Message: proto.String(err.Error()),
+	}
+
+	return r
+}
+
+// NewErrorf returns a Response with special format message.
+func NewErrorf(format string, args ...interface{}) *Response {
+	return NewError(fmt.Errorf(format, args...))
+}

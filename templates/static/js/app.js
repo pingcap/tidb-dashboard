e40ce97d95db0b373b8144015cb5168ba5e9d347
dongxu <huang@pingcap.com>
2016-07-02 22:46:29 +0800

    fe: 1. use static FE resources 2. show buffered events after loading the page

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var dashboardApp = angular.module('dashboardApp', ['ngAnimate']);
-
 // region bg colors
 const colors = [
     "bg-navy",
@@ -22,23 +20,42 @@ const colors = [
     "bg-gray"
 ];
 
+// synchronize with server/event.go
+const eventType = {
+    SPLIT : 1,
+    TRANSFER_LEADER: 2,
+    ADD_REPLICA : 3,
+    REMOVE_REPLICA : 4
+};
+
+const eventStatus = {
+    START : 1,
+    FINISHED : 2
+};
+
+var dashboardApp = angular.module('dashboardApp', ['ngAnimate']);
+
 const eventRowTmpl = `
 <div class="jumbotron" ng-model="event">
     <div class="jumbotron-contents">
         <div class="row">
             <div class="col-sm-1 log-img">
-                <i class="fa fa-scissors fa-2x" ng-if="event.code == 1"></i>
-                <i class="fa fa-exchange fa-2x" ng-if="event.code == 2 && event.status !=2"></i>
-                <i class="fa fa-check fa-2x bg-blue" ng-if="event.code == 2 && event.status ==2"></i>
-                <i class="fa fa-refresh fa-2x" ng-if="event.code == 3 && event.status !=2"></i>
-                <i class="fa fa-check fa-2x bg-green" ng-if="event.code == 3 && event.status ==2"></i>
-                <i class="fa fa-trash fa-2x" ng-if="event.code == 4 && event.status !=2"></i>
-                <i class="fa fa-check fa-2x bg-red" ng-if="event.code == 4 && event.status ==2"></i>
+                <i class="fa fa-scissors fa-2x" ng-if="event.code == eventType.SPLIT"></i>
+
+                <i class="fa fa-exchange fa-2x" ng-if="event.code == eventType.TRANSFER_LEADER && event.status != eventStatus.FINISHED"></i>
+                <i class="fa fa-check fa-2x fa-green" ng-if="event.code == eventType.TRANSFER_LEADER && event.status == eventStatus.FINISHED"></i>
+
+                <i class="fa fa-refresh fa-2x" ng-if="event.code == eventType.ADD_REPLICA && event.status != eventStatus.FINISHED"></i>
+                <i class="fa fa-check fa-2x fa-green" ng-if="event.code == 3 && event.status == eventStatus.FINISHED"></i>
+
+                <i class="fa fa-trash fa-2x" ng-if="event.code == eventType.REMOVE_REPLICA && event.status != eventStatus.FINISHED "></i>
+                <i class="fa fa-check fa-2x fa-green" ng-if="event.code == eventType.REMOVE_REPLICA && event.status == eventStatus.FINISHED"></i>
+
             </div>
             <div class="col-md-10 log-msg">
 
                 <!-- split message -->
-                <div ng-if="event.code == 1">
+                <div ng-if="event.code == eventType.SPLIT">
                     Split
                     <span class="label {{ colors[event.split_event.region % colors.length] }}">Region {{ event.split_event.region  }}</span> into
                     <span class="label {{ colors[event.split_event.left % colors.length] }}">Region {{ event.split_event.left }}</span> and
@@ -46,27 +63,26 @@ const eventRowTmpl = `
                 </div>
 
                 <!-- leader transfer message -->
-                <div ng-if="event.code == 2">
+                <div ng-if="event.code == eventType.TRANSFER_LEADER">
                     Transfer leadership of
                     <span class="label {{ colors[event.transfer_leader_event.region % colors.length] }}">Region {{ event.transfer_leader_event.region }}</span> from 
                     <b>Node {{ event.transfer_leader_event.store_from }}</b> to <b> Node {{ event.transfer_leader_event.store_to }}</b>
-                    <label ng-if="event.status == 2" class="label label-success">Finished</label>
+                    <label ng-if="event.status == eventStatus.FINISHED" class="label label-success">Finished</label>
                 </div>
 
                 <!-- add replica message -->
-                <div ng-if="event.code == 3">
+                <div ng-if="event.code == eventType.ADD_REPLICA">
                     Add Replica for <span class="label {{ colors[event.add_replica_event.region % colors.length] }}"> Region {{ event.add_replica_event.region }}</span> 
                     to <b> Node {{ event.add_replica_event.store }}</b>
-                    <label ng-if="event.status == 2" class="label label-success">Finished</label>
+                    <label ng-if="event.status == eventStatus.FINISHED" class="label label-success">Finished</label>
                 </div>
 
                 <!-- remove replica message -->
-                <div ng-if="event.code == 4">
+                <div ng-if="event.code == eventType.REMOVE_REPLICA">
                     Remove Replica for <span class="label {{ colors[event.remove_replica_event.region % colors.length] }}"> Region {{ event.remove_replica_event.region }}</span> 
                     from <b> Node {{ event.remove_replica_event.store }}</b>
-                    <label ng-if="event.status == 2" class="label label-success">Finished</label>
+                    <label ng-if="event.status == eventStatus.FINISHED" class="label label-success">Finished</label>
                 </div>
-
 
             </div>
         </div>
@@ -79,26 +95,39 @@ dashboardApp.directive('eventRow', function() {
         restrict: 'AE',
         scope: {
             event: '=',
-            colors: '='
         },
         replace: 'true',
-        template: eventRowTmpl
+        template: eventRowTmpl,
+        controller: ['$scope', function($scope) {
+            $scope.eventType = eventType;
+            $scope.eventStatus = eventStatus;
+            $scope.colors = colors;
+        }]
     };
 });
 
+dashboardApp.controller('LogEventController', function LogEventController($scope, $timeout, $http) {
 
-dashboardApp.controller('LogEventController', function LogEventController($scope, $timeout) {
-
-    $scope.colors = colors;
     $scope.logs = [];
 
     $scope.init = function(wsHost) {
             var ws = new WebSocket("ws://" + wsHost + "/ws");
 
             ws.onopen = function(evt) {
+                console.log("ws onopen");
+                $http({
+                    method: 'GET',
+                    url: 'http://' + wsHost + '/api/v1/feed?offset=0',
+                }).then(function(dataResponse) {
+                    if (dataResponse.data != null) {
+                        console.log(dataResponse.data)
+                        $scope.logs = dataResponse.data.reverse();
+                    }
+                });
             }
 
             ws.onclose = function(evt) {
+                console.log("ws onclose");
                 ws = null;
             }
 
@@ -115,6 +144,8 @@ dashboardApp.controller('LogEventController', function LogEventController($scope
             }
 
             ws.onerror = function(evt) {
+                console.log("ws onerror");
+                console.log(evt);
             }
     };
 

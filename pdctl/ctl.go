a45845cc13f029f2ab6ff005848eac335d504ab8
ShuNing <nolouch@gmail.com>
2017-02-23 12:53:45 +0800

    pdctl: add ping (#534)
    * pdctl: add ping

--- pdctl/ctl.go
+++ pdctl/ctl.go
@@ -43,6 +43,7 @@ func init() {
 		command.NewMemberCommand(),
 		command.NewExitCommand(),
 		command.NewLabelCommand(),
+		command.NewPingCommand(),
 		command.NewSchedulerCommand(),
 	)
 	cobra.EnablePrefixMatching = true

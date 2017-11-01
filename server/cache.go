86f60014a6eb69433d8bc63ddf3ca3840960b67f
disksing <i@disksing.com>
2017-11-01 15:43:58 +0800

    server: save region to cache even when save kv failed. (#825) (#829)

--- server/cache.go
+++ server/cache.go
@@ -488,7 +488,9 @@ func (c *clusterInfo) handleRegionHeartbeat(region *core.RegionInfo) error {
 
 	if saveKV && c.kv != nil {
 		if err := c.kv.SaveRegion(region.Region); err != nil {
-			return errors.Trace(err)
+			// Not successfully saved to kv is not fatal, it only leads to longer warm-up
+			// after restart. Here we only log the error then go on updating cache.
+			log.Errorf("[region %d] fail to save region %v: %v", region.GetId(), region, err)
 		}
 	}
 

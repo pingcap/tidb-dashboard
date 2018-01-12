37a003bdda23c828b9929663c3363af50e699015
disksing <i@disksing.com>
2018-01-12 17:29:04 +0800

    vendor: update packages. (#915)

--- vendor/github.com/unrolled/render/render.go
+++ vendor/github.com/unrolled/render/render.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
 const (
@@ -63,6 +65,8 @@ type Options struct {
 	Delims Delims
 	// Appends the given character set to the Content-Type header. Default is "UTF-8".
 	Charset string
+	// If DisableCharset is set to true, it will not append the above Charset value to the Content-Type header. Default is false.
+	DisableCharset bool
 	// Outputs human readable JSON.
 	IndentJSON bool
 	// Outputs human readable XML. Default is false.
@@ -71,8 +75,18 @@ type Options struct {
 	PrefixJSON []byte
 	// Prefixes the XML output with the given bytes.
 	PrefixXML []byte
-	// Allows changing of output to XHTML instead of HTML. Default is "text/html".
+	// Allows changing the binary content type.
+	BinaryContentType string
+	// Allows changing the HTML content type.
 	HTMLContentType string
+	// Allows changing the JSON content type.
+	JSONContentType string
+	// Allows changing the JSONP content type.
+	JSONPContentType string
+	// Allows changing the Text content type.
+	TextContentType string
+	// Allows changing the XML content type.
+	XMLContentType string
 	// If IsDevelopment is set to true, this will recompile the templates on every request. Default is false.
 	IsDevelopment bool
 	// Unescape HTML characters "&<>" to their original values. Default is false.
@@ -85,6 +99,9 @@ type Options struct {
 	RequireBlocks bool
 	// Disables automatic rendering of http.StatusInternalServerError when an error occurs. Default is false.
 	DisableHTTPErrorRendering bool
+	// Enables using partials without the current filename suffix which allows use of the same template in multiple files. e.g {{ partial "carosuel" }} inside the home template will match carosel-home or carosel.
+	// ***NOTE*** - This option should be named RenderPartialsWithoutSuffix as that is what it does. "Prefix" is a typo. Maintaining the existing name for backwards compatibility.
+	RenderPartialsWithoutPrefix bool
 }
 
 // HTMLOptions is a struct for overriding some rendering Options for specific HTML call.
@@ -99,6 +116,7 @@ type Render struct {
 	// Customize Secure with an Options struct.
 	opt             Options
 	templates       *template.Template
+	templatesLk     sync.Mutex
 	compiledCharset string
 }
 
@@ -126,7 +144,9 @@ func (r *Render) prepareOptions() {
 	if len(r.opt.Charset) == 0 {
 		r.opt.Charset = defaultCharset
 	}
-	r.compiledCharset = "; charset=" + r.opt.Charset
+	if r.opt.DisableCharset == false {
+		r.compiledCharset = "; charset=" + r.opt.Charset
+	}
 
 	if len(r.opt.Directory) == 0 {
 		r.opt.Directory = "templates"
@@ -134,8 +154,23 @@ func (r *Render) prepareOptions() {
 	if len(r.opt.Extensions) == 0 {
 		r.opt.Extensions = []string{".tmpl"}
 	}
+	if len(r.opt.BinaryContentType) == 0 {
+		r.opt.BinaryContentType = ContentBinary
+	}
 	if len(r.opt.HTMLContentType) == 0 {
 		r.opt.HTMLContentType = ContentHTML
+	}
+	if len(r.opt.JSONContentType) == 0 {
+		r.opt.JSONContentType = ContentJSON
+	}
+	if len(r.opt.JSONPContentType) == 0 {
+		r.opt.JSONPContentType = ContentJSONP
+	}
+	if len(r.opt.TextContentType) == 0 {
+		r.opt.TextContentType = ContentText
+	}
+	if len(r.opt.XMLContentType) == 0 {
+		r.opt.XMLContentType = ContentXML
 	}
 }
 
@@ -265,6 +300,9 @@ func (r *Render) addLayoutFuncs(name string, binding interface{}) {
 		"block": func(partialName string) (template.HTML, error) {
 			log.Print("Render's `block` implementation is now depericated. Use `partial` as a drop in replacement.")
 			fullPartialName := fmt.Sprintf("%s-%s", partialName, name)
+			if r.TemplateLookup(fullPartialName) == nil && r.opt.RenderPartialsWithoutPrefix {
+				fullPartialName = partialName
+			}
 			if r.opt.RequireBlocks || r.TemplateLookup(fullPartialName) != nil {
 				buf, err := r.execute(fullPartialName, binding)
 				// Return safe HTML here since we are rendering our own template.
@@ -274,6 +312,9 @@ func (r *Render) addLayoutFuncs(name string, binding interface{}) {
 		},
 		"partial": func(partialName string) (template.HTML, error) {
 			fullPartialName := fmt.Sprintf("%s-%s", partialName, name)
+			if r.TemplateLookup(fullPartialName) == nil && r.opt.RenderPartialsWithoutPrefix {
+				fullPartialName = partialName
+			}
 			if r.opt.RequirePartials || r.TemplateLookup(fullPartialName) != nil {
 				buf, err := r.execute(fullPartialName, binding)
 				// Return safe HTML here since we are rendering our own template.
@@ -298,18 +339,18 @@ func (r *Render) prepareHTMLOptions(htmlOpt []HTMLOptions) HTMLOptions {
 }
 
 // Render is the generic function called by XML, JSON, Data, HTML, and can be called by custom implementations.
-func (r *Render) Render(w http.ResponseWriter, e Engine, data interface{}) error {
+func (r *Render) Render(w io.Writer, e Engine, data interface{}) error {
 	err := e.Render(w, data)
-	if err != nil && !r.opt.DisableHTTPErrorRendering {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if hw, ok := w.(http.ResponseWriter); err != nil && !r.opt.DisableHTTPErrorRendering && ok {
+		http.Error(hw, err.Error(), http.StatusInternalServerError)
 	}
 	return err
 }
 
 // Data writes out the raw bytes as binary data.
-func (r *Render) Data(w http.ResponseWriter, status int, v []byte) error {
+func (r *Render) Data(w io.Writer, status int, v []byte) error {
 	head := Head{
-		ContentType: ContentBinary,
+		ContentType: r.opt.BinaryContentType,
 		Status:      status,
 	}
 
@@ -321,7 +362,10 @@ func (r *Render) Data(w http.ResponseWriter, status int, v []byte) error {
 }
 
 // HTML builds up the response from the specified template and bindings.
-func (r *Render) HTML(w http.ResponseWriter, status int, name string, binding interface{}, htmlOpt ...HTMLOptions) error {
+func (r *Render) HTML(w io.Writer, status int, name string, binding interface{}, htmlOpt ...HTMLOptions) error {
+	r.templatesLk.Lock()
+	defer r.templatesLk.Unlock()
+
 	// If we are in development mode, recompile the templates on every HTML request.
 	if r.opt.IsDevelopment {
 		r.compileTemplates()
@@ -349,9 +393,9 @@ func (r *Render) HTML(w http.ResponseWriter, status int, name string, binding in
 }
 
 // JSON marshals the given interface object and writes the JSON response.
-func (r *Render) JSON(w http.ResponseWriter, status int, v interface{}) error {
+func (r *Render) JSON(w io.Writer, status int, v interface{}) error {
 	head := Head{
-		ContentType: ContentJSON + r.compiledCharset,
+		ContentType: r.opt.JSONContentType + r.compiledCharset,
 		Status:      status,
 	}
 
@@ -367,9 +411,9 @@ func (r *Render) JSON(w http.ResponseWriter, status int, v interface{}) error {
 }
 
 // JSONP marshals the given interface object and writes the JSON response.
-func (r *Render) JSONP(w http.ResponseWriter, status int, callback string, v interface{}) error {
+func (r *Render) JSONP(w io.Writer, status int, callback string, v interface{}) error {
 	head := Head{
-		ContentType: ContentJSONP + r.compiledCharset,
+		ContentType: r.opt.JSONPContentType + r.compiledCharset,
 		Status:      status,
 	}
 
@@ -383,9 +427,9 @@ func (r *Render) JSONP(w http.ResponseWriter, status int, callback string, v int
 }
 
 // Text writes out a string as plain text.
-func (r *Render) Text(w http.ResponseWriter, status int, v string) error {
+func (r *Render) Text(w io.Writer, status int, v string) error {
 	head := Head{
-		ContentType: ContentText + r.compiledCharset,
+		ContentType: r.opt.TextContentType + r.compiledCharset,
 		Status:      status,
 	}
 
@@ -397,9 +441,9 @@ func (r *Render) Text(w http.ResponseWriter, status int, v string) error {
 }
 
 // XML marshals the given interface object and writes the XML response.
-func (r *Render) XML(w http.ResponseWriter, status int, v interface{}) error {
+func (r *Render) XML(w io.Writer, status int, v interface{}) error {
 	head := Head{
-		ContentType: ContentXML + r.compiledCharset,
+		ContentType: r.opt.XMLContentType + r.compiledCharset,
 		Status:      status,
 	}
 

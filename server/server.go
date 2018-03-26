b1e624918d5cd3a12a47ef23d7ac5eccd7db4757
Connor <zbk602423539@gmail.com>
2018-03-26 16:49:55 +0800

    fix get all config (#1006)

--- server/server.go
+++ server/server.go
@@ -442,6 +442,7 @@ func (s *Server) GetConfig() *Config {
 		namespaces[name] = *opt.load()
 	}
 	cfg.Namespace = namespaces
+	cfg.LabelProperty = s.scheduleOpt.loadLabelPropertyConfig().clone()
 	return cfg
 }
 

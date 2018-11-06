ebc6d84f4920f4cab69d8585e69a94bf732eb94c
disksing <i@disksing.com>
2018-11-06 11:38:58 +0800

    pdctl: fix decode key (#1308)

--- tools/pd-ctl/pdctl/command/region_command.go
+++ tools/pd-ctl/pdctl/command/region_command.go
@@ -21,6 +21,7 @@ import (
 	"net/url"
 	"os/exec"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -261,19 +262,16 @@ func decodeKey(text string) (string, error) {
 			continue
 		}
 		n := r.Next(1)
+		if len(n) == 0 {
+			return "", io.EOF
+		}
+		// See: https://golang.org/ref/spec#Rune_literals
+		if idx := strings.IndexByte(`abfnrtv\'"`, n[0]); idx != -1 {
+			buf = append(buf, []byte("\a\b\f\n\r\t\v\\'\"")[idx])
+			continue
+		}
+
 		switch n[0] {
-		case '"':
-			buf = append(buf, '"')
-		case '\'':
-			buf = append(buf, '\'')
-		case '\\':
-			buf = append(buf, '\\')
-		case 'n':
-			buf = append(buf, '\n')
-		case 't':
-			buf = append(buf, '\t')
-		case 'r':
-			buf = append(buf, '\r')
 		case 'x':
 			fmt.Sscanf(string(r.Next(2)), "%02x", &c)
 			buf = append(buf, c)

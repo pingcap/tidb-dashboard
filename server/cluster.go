e120222062b32cb0eea174c64cb23134cb9fb90a
Ryan Leung <rleungx@gmail.com>
2019-09-17 10:46:17 +0800

    *: improve label metrics (#1753)
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/cluster.go
+++ server/cluster.go
@@ -71,7 +71,7 @@ type RaftCluster struct {
 	prepareChecker *prepareChecker
 	changedRegions chan *core.RegionInfo
 
-	labelLevelStats *statistics.LabelLevelStatistics
+	labelLevelStats *statistics.LabelStatistics
 	regionStats     *statistics.RegionStatistics
 	storesStats     *statistics.StoresStats
 	hotSpotCache    *statistics.HotSpotCache
@@ -144,7 +144,7 @@ func (c *RaftCluster) initCluster(id id.Allocator, opt *config.ScheduleOption, s
 	c.opt = opt
 	c.storage = storage
 	c.id = id
-	c.labelLevelStats = statistics.NewLabelLevelStatistics()
+	c.labelLevelStats = statistics.NewLabelStatistics()
 	c.storesStats = statistics.NewStoresStats()
 	c.prepareChecker = newPrepareChecker()
 	c.changedRegions = make(chan *core.RegionInfo, defaultChangedRegionsLimit)
@@ -443,7 +443,7 @@ func (c *RaftCluster) processRegionHeartbeat(region *core.RegionInfo) error {
 			if c.regionStats != nil {
 				c.regionStats.ClearDefunctRegion(item.GetId())
 			}
-			c.labelLevelStats.ClearDefunctRegion(item.GetId())
+			c.labelLevelStats.ClearDefunctRegion(item.GetId(), c.GetLocationLabels())
 		}
 
 		// Update related stores.

a12177400a35e71dd3fd5b0422cfb6c06f53361c
Wenxuan <hi@breeswish.org>
2020-01-08 17:04:24 +0800

    Add support for CLI flags (#7)
    Signed-off-by: Breezewish <me@breeswish.org>

--- scripts/embed_ui_assets.sh
+++ scripts/embed_ui_assets.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # This script embeds UI assets into Golang source file. UI assets must be already built
-# before calling this script
+# before calling this script.
+#
+# Available flags:
+# NO_ASSET_BUILD_TAG=1
+#   No build tags will be included in the generated source code.
+# ASSET_BUILD_TAG=X
+#   Customize the build tag of the generated source code. If unspecified, build tag will be "ui_server".
 
 set -euo pipefail
 
@@ -10,12 +16,16 @@ PROJECT_DIR="$(dirname "$DIR")"
 
 # See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
 export GOBIN=$PROJECT_DIR/bin
 export PATH=$GOBIN:$PATH
 
-: "${BUILD_TAG:=ui_server}"
+if [ "${NO_ASSET_BUILD_TAG:-}" = "1" ]; then
+  BUILD_TAG_PARAMETER=""
+else
+  BUILD_TAG_PARAMETER="-tags ${ASSET_BUILD_TAG:-ui_server}"
+fi
 
 echo "+ Preflight check"
 if [ ! -d "ui/build" ]; then
@@ -28,7 +38,8 @@ go install github.com/elazarl/go-bindata-assetfs/go-bindata-assetfs
 go install github.com/go-bindata/go-bindata/go-bindata
 
 echo "+ Embed UI assets"
-go-bindata-assetfs -pkg uiserver -prefix ui -tags $BUILD_TAG ui/build/...
+
+go-bindata-assetfs -pkg uiserver -prefix ui $BUILD_TAG_PARAMETER ui/build/...
 HANDLER_PATH=pkg/uiserver/embedded_assets_handler.go
 mv bindata_assetfs.go $HANDLER_PATH
 echo "  - Assets handler written to $HANDLER_PATH"

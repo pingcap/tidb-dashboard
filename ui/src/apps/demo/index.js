eb0a97a38d63f403bc42dd1c86888faf33a84c20
Wenxuan <hi@breeswish.org>
2020-01-09 17:05:15 +0800

    Add Layouts for FrontEnd (#8)
    Signed-off-by: Breezewish <me@breeswish.org>

---
+++ ui/src/apps/demo/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  id: 'demo',
+  loader: () => import('./app.js'),
+  routerPrefix: '/demo',
+  icon: 'pie-chart',
+  menuTitle: 'Demo 2', // TODO: I18N
+}
---
+++ ui/src/layout/RootComponent.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import { Layout, Menu, Icon } from 'antd';
+import { HashRouter as Router, Redirect } from 'react-router-dom';
+import styles from './RootComponent.module.less';
+
+class App extends React.PureComponent {
+  state = {
+    collapsed: false,
+    activeAppId: null,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  handleRouting = () => {
+    const activeApp = this.props.registry.getActiveApp();
+    if (activeApp) {
+      this.setState({
+        activeAppId: activeApp.id,
+      });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('single-spa:routing-event', this.handleRouting);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('single-spa:routing-event', this.handleRouting);
+  }
+
+  render() {
+    const siderWidth = 260;
+
+    return (
+      <Router><Layout className={styles.container}>
+        <Layout.Sider
+          className={styles.sider}
+          width={siderWidth}
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <Redirect exact from="/" to={this.props.registry.getDefaultRouter()} />
+          <Menu
+            mode="inline"
+            theme="dark"
+            selectedKeys={[this.state.activeAppId]}
+          >
+            {this.props.registry.renderAppMenuItem('home')}
+            {this.props.registry.renderAppMenuItem('demo')}
+            <Menu.SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </Menu.SubMenu>
+          </Menu>
+        </Layout.Sider>
+        <Layout>
+          <Layout.Header
+            className={styles.header}
+            style={{ width: `calc(100% - ${this.state.collapsed ? 80 : siderWidth}px)` }}
+          >
+            <span className={styles.siderTrigger} onClick={this.toggle}>
+              <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+            </span>
+          </Layout.Header>
+          <Layout.Content
+            className={styles.content}
+            style={{ paddingLeft: `${this.state.collapsed ? 80 : siderWidth}px` }}
+          >
+            <div id="__spa_content__"></div>
+          </Layout.Content>
+        </Layout>
+      </Layout></Router>
+    );
+  }
+}
+
+export default App;

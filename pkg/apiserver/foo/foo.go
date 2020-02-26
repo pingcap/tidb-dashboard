ed30ec7b4a393f9304518ac800d5c2d3861a554e
baurine <2008.hbl@gmail.com>
2020-02-26 10:48:26 +0800

    sql-diagnosis: support fold and expand subvalues
    - show category

--- pkg/apiserver/foo/foo.go
+++ pkg/apiserver/foo/foo.go
@@ -35,7 +35,7 @@ func (s *Service) Register(r *gin.RouterGroup, auth *user.AuthService) {
 	endpoint := r.Group("/foo")
 	// endpoint.Use(auth.MWAuthRequired())
 	endpoint.GET("/bar/:name", auth.MWAuthRequired(), s.greetHandler)
-	endpoint.GET("/sql/reports", s.sqlReportHandler)
+	endpoint.GET("/sql-diagnosis", s.sqlDiagnosisHandler)
 }
 
 // @Summary Greet
@@ -53,6 +53,7 @@ func (s *Service) greetHandler(c *gin.Context) {
 }
 
 //////////////////////////
+// TODO: move out
 
 type TableDef struct {
 	Category  []string // The category of the table, such as [TiDB]
@@ -60,7 +61,7 @@ type TableDef struct {
 	CommentEN string   // English Comment
 	CommentCN string   // Chinese comment
 	Column    []string // Column name
-	Rows      []*TableRowDef
+	Rows      []TableRowDef
 }
 
 type TableRowDef struct {
@@ -71,22 +72,90 @@ type TableRowDef struct {
 var (
 	tables = []*TableDef{
 		&TableDef{
-			Category:  []string{"TiDB Monitor", "Time Cosuming"},
-			Title:     "Transaction",
-			CommentCN: "",
-			CommentEN: "",
-			Column:    []string{"type", "total count", "P999", "P99", "P90", "P80"},
-			Rows: []*TableRowDef{
-				&TableRowDef{
+			Category: []string{"Report Header"},
+			Title:    "Report Time Range",
+			Rows: []TableRowDef{
+				TableRowDef{
+					Values: []string{"start time", "2020-02-13 19:30:00 +08:00"},
+				},
+				TableRowDef{
+					Values: []string{"end time", "2020-02-13 19:40:00 +08:00"},
+				},
+			},
+		},
+		&TableDef{
+			Title:  "Cluster Typo and Machine Hardware Information",
+			Column: []string{"host", "instance", "CPU Cores", "Memory (GB)", "Disk (GB)", "server time"},
+			Rows: []TableRowDef{
+				TableRowDef{
+					Values: []string{"172.16.x.x", "tidb,pd,tikv", "28/56", "128", "/dev/nvme0:1000 /dev/nvme2:2000", "24h"},
+				},
+				TableRowDef{
+					Values: []string{"5.85", "tidb", "", "", "", ""},
+				},
+			},
+		},
+		&TableDef{
+			Title:  "Version Info",
+			Column: []string{"instance", "version", "Git hash"},
+			Rows: []TableRowDef{
+				TableRowDef{
+					Values: []string{"TiDB", "V3.0.8", "xxx"},
+				},
+				TableRowDef{
+					Values: []string{"TiDB", "V3.0.7", "yyy"},
+				},
+				TableRowDef{
+					Values: []string{"TiKV", "V3.0.8", "zzz"},
+				},
+				TableRowDef{
+					Values: []string{"PD", "V3.0.8", "nnn"},
+				},
+			},
+		},
+		&TableDef{
+			Category: []string{"TiDB Monitor"},
+			Title:    "Time Cosuming",
+			Column:   []string{"metric name", "metric label", "ratio", "total time", "total count", "p9999", "p99", "p90", "p80"},
+			Rows: []TableRowDef{
+				TableRowDef{
+					Values: []string{"SQL Query", "", "/", "100", "200", "2", "1", "0.8", "0.5"},
+					SubValues: [][]string{
+						[]string{"", "select", "0.8", "80", "25", "1.5", "0.9", "0.6", "0.4"},
+						[]string{"", "insert", "0.1", "10", "", "", "", "", ""},
+						[]string{"", "update", "0.05", "5", "", "", "", "", ""},
+					},
+				},
+				TableRowDef{
+					Values: []string{"Slow Query", "", "", "20", "10", "", "", "", ""},
+				},
+				TableRowDef{
+					Values: []string{"parse", "", "", "10", "200", "", "", "", ""},
+				},
+				TableRowDef{
+					Values: []string{"pd cmd time", "", "/", "100", "200", "2", "1", "0.8", "0.5"},
+					SubValues: [][]string{
+						[]string{"", "tso", "0.8", "80", "25", "1.5", "0.9", "0.6", "0.4"},
+						[]string{"", "wait", "0.1", "10", "", "", "", "", ""},
+						[]string{"", "scan region", "0.05", "5", "", "", "", "", ""},
+					},
+				},
+			},
+		},
+		&TableDef{
+			Title:  "Transaction",
+			Column: []string{"", "total count", "P999", "P99", "P90", "P80"},
+			Rows: []TableRowDef{
+				TableRowDef{
 					Values: []string{"txn statement num", "3000", "150", "100", "60", "40"},
 				},
-				&TableRowDef{
+				TableRowDef{
 					Values: []string{"txn write keys num", "60000", "", "", "", ""},
 				},
-				&TableRowDef{
+				TableRowDef{
 					Values: []string{"txn write regions num", "1000", "", "", "", ""},
 				},
-				&TableRowDef{
+				TableRowDef{
 					Values: []string{"load safepoint num", "200", "/", "/", "/", "/"},
 					SubValues: [][]string{
 						[]string{"", "ok", "200", "", "", ""},
@@ -100,18 +169,18 @@ var (
 			Title:     "Cluster Status",
 			CommentCN: "对比一段时间内集群状态变化",
 			CommentEN: "Compare the cluster status changes",
-			Column:    []string{"type", "min value", "max value"},
-			Rows: []*TableRowDef{
-				&TableRowDef{
+			Column:    []string{"", "min value", "max value"},
+			Rows: []TableRowDef{
+				TableRowDef{
 					Values: []string{"store disconnected count", "0", "1"},
 				},
-				&TableRowDef{
+				TableRowDef{
 					Values: []string{"store up count", "4", "5"},
 				},
-				&TableRowDef{
+				TableRowDef{
 					Values: []string{"leader count", "21", "739"},
 				},
-				&TableRowDef{
+				TableRowDef{
 					Values: []string{"region count", "21", "739"},
 				},
 			},
@@ -119,6 +188,6 @@ var (
 	}
 )
 
-func (s *Service) sqlReportHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "sql-report/index", tables)
+func (s *Service) sqlDiagnosisHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "sql-diagnosis/index", tables)
 }
--- ui/src/apps/demo/RootComponent.js
+++ ui/src/apps/demo/RootComponent.js
@@ -7,15 +7,25 @@ import client from '@/utils/client';
 
 const App = () => (
   <Router>
-    <Button type="primary" onClick={handleClick}>Button</Button>
     <Link to="/home">
       <Button type="primary">Go To Home</Button>
     </Link>
+    <Button type="primary" onClick={handleClick}>
+      Button
+    </Button>
+    <Button type="primary">
+      <a
+        href="http://127.0.0.1:12333/dashboard/api/foo/sql-diagnosis"
+        target="_blank"
+      >
+        SQL Diagnosis Report
+      </a>
+    </Button>
   </Router>
 );
 
 async function handleClick() {
-  const r = await client.dashboard.fooBarNameGet("abc");
+  const r = await client.dashboard.fooBarNameGet('abc');
   alert(r.data);
 }
 

8a2cacaf191fbbcb11ca0ad55d36c316bc2e9dd2
reafans <30926443+reafans@users.noreply.github.com>
2020-03-06 22:03:58 +0800

    sql-dignosis：fix the conflict (#184)
    * update
    
    * fix bug
    
    * fix progress back bug
    
    * Add parrallel in compare table and add max_diff_ratio table. (#183)
    
    * add parralell in compare and add max_diff table
    
    Signed-off-by: crazycs <crazycs520@gmail.com>
    
    * speed up
    
    Signed-off-by: crazycs <crazycs520@gmail.com>
    
    Co-authored-by: crazycs <crazycs520@gmail.com>

--- pkg/apiserver/diagnose/report_test.go
+++ pkg/apiserver/diagnose/report_test.go
@@ -59,22 +59,22 @@ func (t *testReportSuite) TestGetTable(c *C) {
 	printRows(&table)
 }
 
-func (t *testReportSuite) TestGetCompareTable(c *C) {
-	cli, err := gorm.Open("mysql", "root:@tcp(172.16.5.40:4009)/test?charset=utf8&parseTime=True&loc=Local")
-	c.Assert(err, IsNil)
-	defer cli.Close()
-
-	startTime1 := "2020-03-03 17:08:00"
-	endTime1 := "2020-03-03 17:11:00"
-
-	startTime2 := "2020-03-03 17:18:00"
-	endTime2 := "2020-03-03 17:21:00"
-
-	tables := GetCompareReportTables(startTime1, endTime1, startTime2, endTime2, cli)
-	for _, tbl := range tables {
-		printRows(tbl)
-	}
-}
+//func (t *testReportSuite) TestGetCompareTable(c *C) {
+//	cli, err := gorm.Open("mysql", "root:@tcp(172.16.5.40:4009)/test?charset=utf8&parseTime=True&loc=Local")
+//	c.Assert(err, IsNil)
+//	defer cli.Close()
+//
+//	startTime1 := "2020-03-03 17:08:00"
+//	endTime1 := "2020-03-03 17:11:00"
+//
+//	startTime2 := "2020-03-03 17:18:00"
+//	endTime2 := "2020-03-03 17:21:00"
+//
+//	tables := GetCompareReportTablesForDisplay(startTime1, endTime1, startTime2, endTime2, cli)
+//	for _, tbl := range tables {
+//		printRows(tbl)
+//	}
+//}
 
 func (t *testReportSuite) TestCompareTable(c *C) {
 	table1 := TableDef{
@@ -141,12 +141,13 @@ func (t *testReportSuite) TestCompareTable(c *C) {
 		},
 	}
 
+	dr := &diffRows{}
 	for _, cas := range cases {
 		t1 := table1
 		t2 := table1
 		t1.Rows = cas.rows1
 		t2.Rows = cas.rows2
-		t, err := compareTable(&t1, &t2)
+		t, err := compareTable(&t1, &t2, dr)
 		c.Assert(err, IsNil)
 		c.Assert(len(t.Rows), Equals, len(cas.out))
 		for i, row := range t.Rows {

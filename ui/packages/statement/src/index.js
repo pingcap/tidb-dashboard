114cff3d9c5da555d8b293609284176c48376c3a
baurine <2008.hbl@gmail.com>
2020-03-13 18:22:53 +0800

    wip

--- ui/packages/statement/src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './translations'
-
-export * from './components'
--- ui/packages/statement/src/translations/index.js
+++
@@ -1,8 +0,0 @@
-// notice: @rollup/plugin-yaml only can be used in .js file, can't use it in .ts file
-import en from './en.yaml'
-import zh from './zh-CN.yaml'
-
-export const translation = {
-  en,
-  'zh-CN': zh,
-}

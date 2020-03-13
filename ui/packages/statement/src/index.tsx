87f0e16bff535cec21e09a3f8999e1d5cdda0113
baurine <2008.hbl@gmail.com>
2020-03-13 18:30:30 +0800

    wip

--- ui/packages/statement/src/index.tsx
+++ ui/packages/statement/src/index.tsx
@@ -1,7 +1,5 @@
 // import * as en from './translations/en.yaml'
 
-const en = require('./translations/en.yaml')
-
 export * from './components'
 
-export { en }
+export * from './translations'
---
+++ ui/packages/statement/src/translations/index.js
@@ -0,0 +1,7 @@
+import en from './en.yaml'
+import zh from './zh-CN.yaml'
+
+export const translation = {
+  en,
+  'zh-CN': zh,
+}

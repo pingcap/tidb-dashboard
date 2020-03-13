c412e48ccc508f3ef1c1f7cdea6198f56479b128
baurine <2008.hbl@gmail.com>
2020-03-13 16:58:19 +0800

    wip

--- ui/packages/statement/rollup.config.js
+++ ui/packages/statement/rollup.config.js
@@ -2,6 +2,8 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import yaml from '@rollup/plugin-yaml'
+import postcss from 'rollup-plugin-postcss'
+import external from 'rollup-plugin-peer-deps-external'
 import pkg from './package.json'
 
 export default [
@@ -12,6 +14,13 @@ export default [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
     ],
-    plugins: [resolve(), commonjs(), typescript(), yaml()],
+    plugins: [
+      external(),
+      postcss({ modules: true }),
+      resolve(),
+      commonjs(),
+      typescript(),
+      yaml(),
+    ],
   },
 ]

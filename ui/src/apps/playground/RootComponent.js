ca4dd4cfc52ebc492420e01450cec9a560c847ed
baurine <2008.hbl@gmail.com>
2020-03-15 20:53:06 +0800

    update playground

--- ui/src/apps/playground/RootComponent.js
+++ ui/src/apps/playground/RootComponent.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { HashRouter as Router } from 'react-router-dom'
-import StatementsOverviewPage from './StatementsOverviewPage'
+import { StatementsOverviewPage } from '@pingcap-incubator/statement'
+import client from '@/utils/client'
 
 const App = () => (
   <Router>
     <div style={{ margin: 12 }}>
-      <StatementsOverviewPage />
+      <StatementsOverviewPage
+        dashboardClient={client.dashboard}
+        detailPagePath="/statement/detail"
+      />
     </div>
   </Router>
 )

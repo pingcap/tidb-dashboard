f51ed8844bd0e87eed11afdcd867bcae642d526f
baurine <2008.hbl@gmail.com>
2020-03-18 09:10:43 +0800

    declare the yaml module

--- ui/packages/statement/src/typings.d.ts
+++ ui/packages/statement/src/typings.d.ts
@@ -7,6 +7,11 @@ declare module '*.css' {
   export default content;
 }
 
+declare module '*.yaml' {
+  const content: any;
+  export default content;
+}
+
 interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 
 declare module '*.svg' {

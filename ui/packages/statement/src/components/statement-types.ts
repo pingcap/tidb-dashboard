d23f7eeceed6e80f511f19b23c17e5b389cd126a
baurine <2008.hbl@gmail.com>
2020-03-18 09:15:52 +0800

    polish type export

--- ui/packages/statement/src/components/statement-types.ts
+++ ui/packages/statement/src/components/statement-types.ts
@@ -13,19 +13,14 @@ export interface StatementConfig {
 }
 
 //////////////////
-import {
-  StatementTimeRange as D_StatementTimeRange,
-  StatementOverview as D_StatementOverview,
-  StatementNode as D_StatementNode,
-  StatementDetail as D_SattementDetail,
-  StatementPlan as D_StatementPlan,
-} from '@pingcap-incubator/dashboard_client'
 
-export type StatementTimeRange = D_StatementTimeRange
-export type StatementOverview = D_StatementOverview
-export type StatementDetailInfo = D_SattementDetail
-export type StatementNode = D_StatementNode
-export type StatementPlan = D_StatementPlan
+export {
+  StatementTimeRange,
+  StatementOverview,
+  StatementNode,
+  StatementDetail as StatementDetailInfo,
+  StatementPlan,
+} from '@pingcap-incubator/dashboard_client'
 
 export interface StatementPlanStep {
   id: string

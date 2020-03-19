5af47fe984d79f9c3c0849086e2d2fa12f4d3fb6
baurine <2008.hbl@gmail.com>
2020-03-19 14:01:27 +0800

    revert toolbar and style

--- ui/packages/keyvis/src/components/ToolBar.tsx
+++ ui/packages/keyvis/src/components/ToolBar.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Slider, Spin, Icon, Select, Dropdown, Button } from 'antd';
-import { withTranslation, WithTranslation } from 'react-i18next';
-import Flexbox from '@g07cha/flexbox-react';
+import React, { Component } from 'react'
+import { Slider, Spin, Icon, Select, Dropdown, Button } from 'antd'
+import { withTranslation, WithTranslation } from 'react-i18next'
+import Flexbox from '@g07cha/flexbox-react'
 
 export interface IKeyVisToolBarProps {
   isLoading: boolean
@@ -84,111 +84,107 @@ class KeyVisToolBar extends Component<IKeyVisToolBarProps & WithTranslation> {
     ]
 
     return (
-      <Card>
-        <div className="PD-KeyVis-Toolbar">
-          <Dropdown
-            overlay={
-              <div id="PD-KeyVis-Brightness-Overlay">
-                <div
-                  onClick={e => {
-                    e.stopPropagation()
-                  }}
-                >
-                  <Flexbox flexDirection="column" alignItems="center">
-                    <div className="PD-Cluster-Legend" />
-                    <Slider
-                      style={{ width: 360 }}
-                      defaultValue={0}
-                      min={-6}
-                      max={6}
-                      step={0.1}
-                      onChange={value =>
-                        this.handleBrightLevel(value as number)
-                      }
-                    />
-                  </Flexbox>
-                </div>
+      <div className="PD-KeyVis-Toolbar">
+        <Dropdown
+          overlay={
+            <div id="PD-KeyVis-Brightness-Overlay">
+              <div
+                onClick={e => {
+                  e.stopPropagation()
+                }}
+              >
+                <Flexbox flexDirection="column" alignItems="center">
+                  <div className="PD-Cluster-Legend" />
+                  <Slider
+                    style={{ width: 360 }}
+                    defaultValue={0}
+                    min={-6}
+                    max={6}
+                    step={0.1}
+                    onChange={value => this.handleBrightLevel(value as number)}
+                  />
+                </Flexbox>
               </div>
-            }
-            trigger={['click']}
-            onVisibleChange={this.handleBrightnessDropdown}
-          >
-            <Button icon="bulb">
-              {t('keyvis.toolbar.brightness')}
-              <Icon type="down" />
-            </Button>
-          </Dropdown>
-
-          <div className="space" />
-
-          <Button.Group>
-            <Button
-              onClick={this.props.onToggleBrush}
-              icon="arrows-alt"
-              type={isOnBrush ? 'primary' : 'default'}
-            >
-              {t('keyvis.toolbar.zoom.select')}
-            </Button>
-            <Button onClick={this.props.onResetZoom}>
-              {t('keyvis.toolbar.zoom.reset')}
-            </Button>
-          </Button.Group>
-
-          <div className="space" />
-
-          <Button
-            onClick={this.handleAutoFetch}
-            icon="sync"
-            type={isAutoFetch ? 'primary' : 'default'}
-          >
-            {t('keyvis.toolbar.auto_refresh')}
+            </div>
+          }
+          trigger={['click']}
+          onVisibleChange={this.handleBrightnessDropdown}
+        >
+          <Button icon="bulb">
+            {t('keyvis.toolbar.brightness')}
+            <Icon type="down" />
           </Button>
+        </Dropdown>
 
-          <div className="space" />
+        <div className="space" />
 
-          <Select
-            onChange={this.handleDateRange}
-            value={dateRange}
-            style={{ width: 150 }}
+        <Button.Group>
+          <Button
+            onClick={this.props.onToggleBrush}
+            icon="arrows-alt"
+            type={isOnBrush ? 'primary' : 'default'}
           >
-            {DateRangeOptions.map(option => (
-              <Select.Option
-                key={option.text}
-                value={option.value}
-                className="PD-KeyVis-Select-Option"
-              >
-                <Icon type="clock-circle" /> {option.text}
-              </Select.Option>
-            ))}
-          </Select>
+            {t('keyvis.toolbar.zoom.select')}
+          </Button>
+          <Button onClick={this.props.onResetZoom}>
+            {t('keyvis.toolbar.zoom.reset')}
+          </Button>
+        </Button.Group>
 
-          <div className="space" />
+        <div className="space" />
 
-          <Select
-            onChange={this.handleMetricChange}
-            value={metricType}
-            style={{ width: 160 }}
-          >
-            {MetricOptions.map(option => (
-              <Select.Option
-                key={option.text}
-                value={option.value}
-                className="PD-KeyVis-Select-Option"
-              >
-                <Icon type="area-chart" /> {option.text}
-              </Select.Option>
-            ))}
-          </Select>
+        <Button
+          onClick={this.handleAutoFetch}
+          icon="sync"
+          type={isAutoFetch ? 'primary' : 'default'}
+        >
+          {t('keyvis.toolbar.auto_refresh')}
+        </Button>
 
-          <div className="space" />
+        <div className="space" />
 
-          {this.props.isLoading && (
-            <Spin
-              indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}
-            />
-          )}
-        </div>
-      </Card>
+        <Select
+          onChange={this.handleDateRange}
+          value={dateRange}
+          style={{ width: 150 }}
+        >
+          {DateRangeOptions.map(option => (
+            <Select.Option
+              key={option.text}
+              value={option.value}
+              className="PD-KeyVis-Select-Option"
+            >
+              <Icon type="clock-circle" /> {option.text}
+            </Select.Option>
+          ))}
+        </Select>
+
+        <div className="space" />
+
+        <Select
+          onChange={this.handleMetricChange}
+          value={metricType}
+          style={{ width: 160 }}
+        >
+          {MetricOptions.map(option => (
+            <Select.Option
+              key={option.text}
+              value={option.value}
+              className="PD-KeyVis-Select-Option"
+            >
+              <Icon type="area-chart" /> {option.text}
+            </Select.Option>
+          ))}
+        </Select>
+
+        <div className="space" />
+
+        {this.props.isLoading && (
+          <Spin
+            indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}
+          />
+        )}
+      </div>
     )
   }
 }

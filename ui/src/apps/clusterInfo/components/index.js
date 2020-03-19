e2a55c15ccb343897f411f1af4b7d4557d3b50ce
Wenxuan <hi@breeswish.org>
2020-03-19 11:07:18 +0800

    clusterinfo: Extract cluster into standalone component (#239)
    Signed-off-by: Breezewish <me@breeswish.org>

--- ui/src/apps/clusterInfo/components/index.js
+++
@@ -1,5 +0,0 @@
-import ComponentPanel from './ComponentPanel'
-import ClusterInfoTable from './ClusterInfoTable'
-import MonitorAlertBar from './MonitorAlertBar'
-
-export { ComponentPanel, ClusterInfoTable, MonitorAlertBar }

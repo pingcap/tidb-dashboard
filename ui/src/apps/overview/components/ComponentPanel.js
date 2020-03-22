c8fdaa02b96f9fff0199115785f9d16174027d1d
mapleFU <1506118561@qq.com>
2020-03-22 18:05:39 +0800

    [ADD] add same colour checking
    Signed-off-by: mapleFU <1506118561@qq.com>

--- ui/src/apps/overview/components/ComponentPanel.js
+++ ui/src/apps/overview/components/ComponentPanel.js
@@ -61,7 +61,8 @@ function ComponentPanel({ data, field }) {
           </Col>
           <Col span={9}>
             <div className={styles.desc}>{t('overview.status.abnormal')}</div>
-            <div className={abnormal_nodes === 0 ? styles.alive : styles.down}>
+            {/*Note: If `field_error` is true, both "up" and "down" should be "-" with the sample color*/}
+            <div className={abnormal_nodes === 0 || !field_error ? styles.alive : styles.down}>
               {abnormal_nodes}
             </div>
           </Col>

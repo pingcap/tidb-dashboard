226f48c307da914363b4b889f818b4ee7de354ba
baurine <2008.hbl@gmail.com>
2020-03-25 16:31:51 +0800

    fix typo

--- ui/tests/e2e/test_config.ts
+++ ui/tests/e2e/test_config.ts
@@ -1,7 +1,7 @@
 export let SERVER_URL = `${
   process.env.SERVER_URL || 'http://localhost:3000/'
 }#/`
-export const LOGIN_URL = SERVER_URL + 'sigin'
+export const LOGIN_URL = SERVER_URL + 'signin'
 export const OVERVIEW_URL = SERVER_URL + 'overview'
 
 export const PUPPETEER_CONFIG = process.env.CI

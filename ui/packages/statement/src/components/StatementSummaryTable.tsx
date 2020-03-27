def394e40ec95fd22c583ed7ea4946895f114687
baurine <2008.hbl@gmail.com>
2020-03-27 14:17:54 +0800

    only import sql syntax highlight instead of importing all languages to reduce bundle size

--- ui/packages/statement/src/components/StatementSummaryTable.tsx
+++ ui/packages/statement/src/components/StatementSummaryTable.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Table } from 'antd'
 import moment from 'moment'
-import { StatementDetailInfo } from './statement-types'
 import { useTranslation } from 'react-i18next'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import sqlFormatter from 'sql-formatter-plus'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import sql from 'react-syntax-highlighter/dist/esm/languages/hljs/sql'
+import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { StatementDetailInfo } from './statement-types'
+
+SyntaxHighlighter.registerLanguage('sql', sql)
 
 type align = 'left' | 'right' | 'center'
 

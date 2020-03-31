d6656eff87366229f73267a2439776c12a345d55
baurine <2008.hbl@gmail.com>
2020-03-31 22:50:25 +0800

    wip

--- ui/packages/diagnosis_report/src/index.tsx
+++ ui/packages/diagnosis_report/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import DiagnosisReport from './components/DiagnosisReport'
 
 import 'bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+
+import * as i18nUtil from './utils/i18n'
+import DiagnosisReport from './components/DiagnosisReport'
 import './index.css'
 
 console.log(window.__diagnosis_data__)
+
+i18nUtil.init()
 
 ReactDOM.render(<DiagnosisReport />, document.getElementById('root'))
---
+++ ui/packages/diagnosis_report/src/utils/i18n.js
@@ -0,0 +1,37 @@
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { translations as diagnosisTrans } from '../translations'
+
+export function addTranslations(langs) {
+  Object.keys(langs).forEach((key) => {
+    const translations = langs[key]
+    addTranslationResource(key, translations)
+  })
+  return
+}
+
+export function addTranslationResource(lang, translations) {
+  i18next.addResourceBundle(lang, 'translation', translations, true, false)
+}
+
+export function init() {
+  i18next
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      resources: {},
+      fallbackLng: 'en',
+      interpolation: {
+        escapeValue: false,
+      },
+    })
+    .then(() => {
+      addTranslations(diagnosisTrans)
+    })
+}
+
+export const ALL_LANGUAGES = {
+  en: 'English',
+  'zh-CN': '简体中文',
+}

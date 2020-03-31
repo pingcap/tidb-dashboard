f2cbd0b7683ff23605f77f0f6507f835ef351b51
baurine <2008.hbl@gmail.com>
2020-03-31 18:44:32 +0800

    wip

--- ui/packages/diagnosis_report/src/index.tsx
+++ ui/packages/diagnosis_report/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bulma/css/bulma.css'
 import Report from './Report'
+
+import 'bulma/css/bulma.css'
+import './index.css'
 
 ReactDOM.render(<Report />, document.getElementById('root'))

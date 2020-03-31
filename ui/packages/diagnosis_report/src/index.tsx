f79122b77471ce237667e8989ecf4c48a5af5203
baurine <2008.hbl@gmail.com>
2020-03-31 18:31:25 +0800

    wip

--- ui/packages/diagnosis_report/src/index.tsx
+++ ui/packages/diagnosis_report/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bulma/css/bulma.css'
-import './index.css'
-import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+function App() {
+  return <h1>SQL Diagnosis Report</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

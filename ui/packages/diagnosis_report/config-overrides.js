124c95b6b35eb7d5ebb9202cdca41693b9879879
baurine <2008.hbl@gmail.com>
2020-04-01 17:33:47 +0800

    wip

--- ui/packages/diagnosis_report/config-overrides.js
+++ ui/packages/diagnosis_report/config-overrides.js
@@ -1,4 +1,14 @@
 const { override } = require('customize-cra')
 const addYaml = require('react-app-rewire-yaml')
+const reactAppRewireBuildDev = require('react-app-rewire-build-dev')
 
-module.exports = override(addYaml)
+const options = {
+  outputPath: './build',
+  basename: '/dashboard/api/diagnose/assets/',
+}
+
+function watchDev(config, env) {
+  return reactAppRewireBuildDev(config, env, options)
+}
+
+module.exports = override(addYaml, watchDev)

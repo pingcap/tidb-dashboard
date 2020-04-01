835a059b35a2ca2bb71c03fc3ddd3202802c74cf
baurine <2008.hbl@gmail.com>
2020-04-01 19:34:54 +0800

    wip

--- ui/packages/diagnosis_report/config-overrides.js
+++ ui/packages/diagnosis_report/config-overrides.js
@@ -1,15 +1,4 @@
 const { override } = require('customize-cra')
 const addYaml = require('react-app-rewire-yaml')
 
-const watchDev = () => (config) => {
-  // config.mode = 'development'
-  // config.devtool = 'eval-cheap-module-source-map'
-  // delete config.optimization
-  // config.watch = true
-  // config.watchOptions = {
-  //   ignored: /node_modules/,
-  // }
-  return config
-}
-
-module.exports = override(watchDev(), addYaml)
+module.exports = override(addYaml)

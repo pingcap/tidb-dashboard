b01f019fe06a0b2c3fbef187420bee08dd825c52
baurine <2008.hbl@gmail.com>
2020-04-02 11:00:06 +0800

    fix relatve path issue

--- pkg/apiserver/diagnose/assets_dev.go
+++ pkg/apiserver/diagnose/assets_dev.go
@@ -3,8 +3,19 @@
 package diagnose
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
+func getWd() string {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	return path
+}
+
+// relative path only works in dev mode, can't work by `go generate pkg/apiserver/diagnose/assets_embed.go`
 // var Vfs http.FileSystem = http.Dir("ui/packages/diagnosis_report/build")
-var Vfs http.FileSystem = http.Dir("/Users/baurine/Codes/Work/pingcap-incubator/tidb-dashboard/ui/packages/diagnosis_report/build")
+var Vfs http.FileSystem = http.Dir(getWd() + "/../../../ui/packages/diagnosis_report/build")
--- pkg/apiserver/diagnose/assets_embed.go
+++ pkg/apiserver/diagnose/assets_embed.go
@@ -1,5 +1,7 @@
 //go:generate vfsgendev -source="github.com/pingcap-incubator/tidb-dashboard/pkg/apiserver/diagnose".Vfs
 
+// run `go generate pkg/apiserver/diagnose/assets_embed.go` in the project root folder to trigger above command
+
 package diagnose
 
 // This file is only a placeholder for invoking go:generate.

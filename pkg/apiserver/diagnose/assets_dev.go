31f17af36d8ba1ae2bb26dbebc3cc7659844a95b
baurine <2008.hbl@gmail.com>
2020-04-02 11:49:25 +0800

    fix path

--- pkg/apiserver/diagnose/assets_dev.go
+++ pkg/apiserver/diagnose/assets_dev.go
@@ -6,16 +6,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func getWd() string {
+func getDiagnosisAssetsPath() string {
 	path, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
 	}
-	return path
+	var assetsPath string
+	if strings.HasSuffix(path, "tidb-dashboard") {
+		// dev mode
+		assetsPath = path + "/ui/packages/diagnosis_report/build"
+	} else if strings.HasSuffix(path, "diagnose") {
+		// work with `go generate ./pkg/apiserver/diagnose` in the project root folder
+		assetsPath = path + "/../../../ui/packages/diagnosis_report/build"
+	}
+	return assetsPath
 }
 
 // relative path only works in dev mode, can't work by `go generate ./pkg/apiserver/diagnose`
 // var Vfs http.FileSystem = http.Dir("ui/packages/diagnosis_report/build")
-var Vfs http.FileSystem = http.Dir(getWd() + "/../../../ui/packages/diagnosis_report/build")
+
+var Vfs http.FileSystem = http.Dir(getDiagnosisAssetsPath())

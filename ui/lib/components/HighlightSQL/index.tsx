e31269c174c859107e81bd5d3083c10454ee1fd0
Breezewish <me@breeswish.org>
2020-05-09 12:17:53 +0800

    Cross-instance memorization for HighlightSQL

--- ui/lib/components/HighlightSQL/index.tsx
+++ ui/lib/components/HighlightSQL/index.tsx
@@ -4,9 +4,9 @@ import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import sql from 'react-syntax-highlighter/dist/esm/languages/hljs/sql'
 import lightTheme from 'react-syntax-highlighter/dist/esm/styles/hljs/atom-one-light'
 import darkTheme from 'react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark-reasonable'
-
 import Pre from '../Pre'
 import formatSql from '@lib/utils/formatSql'
+import moize from 'moize'
 
 SyntaxHighlighter.registerLanguage('sql', sql)
 
@@ -51,4 +51,6 @@ function HighlightSQL({ sql, compact, theme = 'light' }: Props) {
   )
 }
 
-export default React.memo(HighlightSQL)
+export default moize.react(HighlightSQL, {
+  maxSize: 1000,
+})

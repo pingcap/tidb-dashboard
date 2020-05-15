e5041ed1a3d5398e96a649aec0593d466f7de302
baurine <2008.hbl@gmail.com>
2020-05-15 14:24:16 +0800

    instance_profiling: refine, remove Math.random() as key

--- ui/lib/apps/InstanceProfiling/index.tsx
+++ ui/lib/apps/InstanceProfiling/index.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from 'react-router-dom'
 
 import { Root } from '@lib/components'
+import { Detail, List } from './pages'
 
-import DetailPage from './pages/Detail'
-import ListPage from './pages/List'
+function DetailPageWrapper() {
+  const { id } = useParams()
+
+  return <Detail key={id} />
+}
 
 const App = () => (
   <Root>
     <Router>
       <Routes>
-        <Route
-          exact
-          path="/instance_profiling"
-          element={<ListPage key={Math.random()} />}
-        />
-        <Route
-          path="/instance_profiling/:id"
-          element={<DetailPage key={Math.random()} />}
-        />
+        <Route path="/instance_profiling" element={<List />} />
+        <Route path="/instance_profiling/:id" element={<DetailPageWrapper />} />
       </Routes>
     </Router>
   </Root>

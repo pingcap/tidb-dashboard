79d198ac397aaa85e5c2db49aa067c8c85c367f7
niedhui <niedhui@gmail.com>
2020-05-21 00:32:06 +0800

    gzip static server
    Signed-off-by: niedhui <niedhui@gmail.com>

--- pkg/uiserver/uiserver.go
+++ pkg/uiserver/uiserver.go
@@ -14,8 +14,12 @@
 package uiserver
 
 import (
+	"compress/gzip"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
+	"sync"
 
 	assetfs "github.com/elazarl/go-bindata-assetfs"
 )
@@ -27,7 +31,7 @@ var (
 func Handler() http.Handler {
 	if fs != nil {
 		fileServer := http.FileServer(fs)
-		return fileServer
+		return newGzipHandler(fileServer)
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -37,4 +41,53 @@ func Handler() http.Handler {
 
 func AssetFS() *assetfs.AssetFS {
 	return fs
+}
+
+type gzipHandler struct {
+	fs http.Handler  // The original FileServer
+	pool sync.Pool
+}
+
+func newGzipHandler(fs http.Handler) http.Handler {
+	var pool sync.Pool
+	pool.New = func() interface{} {
+		gz, err := gzip.NewWriterLevel(ioutil.Discard, gzip.BestSpeed)
+		if err != nil {
+			panic(err)
+		}
+		return gz
+	}
+	return &gzipHandler{fs: fs, pool: pool}
+}
+
+func (g *gzipHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if g.shouldCompress(r) {
+		gz := g.pool.Get().(*gzip.Writer)
+		defer func() {
+			gz.Close()
+			gz.Reset(ioutil.Discard)
+			g.pool.Put(gz)
+		}()
+		gz.Reset(w)
+		w.Header().Set("Content-Encoding", "gzip")
+		g.fs.ServeHTTP(&gzipWriter{
+			ResponseWriter: w,
+			writer:         gz,
+		}, r)
+		return
+	}
+	g.fs.ServeHTTP(w, r)
+}
+
+func (g *gzipHandler) shouldCompress(r *http.Request) bool {
+	return strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
+}
+
+type gzipWriter struct {
+	http.ResponseWriter
+	writer *gzip.Writer
+}
+
+func (g *gzipWriter) Write(data []byte) (int, error) {
+	return g.writer.Write(data)
 }

13f048e43179d2862cc555a8a28833492be76e26
Fullstop000 <fullstop1005@gmail.com>
2020-05-26 00:26:50 +0800

    Support highlight log searching results (#522)
    Signed-off-by: Fullstop000 <fullstop1005@gmail.com>

--- ui/lib/apps/SearchLogs/components/Log.tsx
+++ ui/lib/apps/SearchLogs/components/Log.tsx
@@ -1,14 +1,16 @@
 import React, { useCallback } from 'react'
+import Highlighter from 'react-highlight-words'
 import { TextWrap, Pre } from '@lib/components'
 
 import styles from './Styles.module.css'
 
 interface LogProps {
+  patterns: string[]
   expanded: boolean
   log: string
 }
 
-export default function Log({ log, expanded }: LogProps) {
+export default function Log({ patterns, log, expanded }: LogProps) {
   const handleClick = useCallback((ev: React.MouseEvent<HTMLDivElement>) => {
     ev.stopPropagation()
   }, [])
@@ -18,7 +20,12 @@ export default function Log({ log, expanded }: LogProps) {
       onClick={handleClick}
       className={styles.logText}
     >
-      <Pre>{log}</Pre>
+      <Pre>
+        <Highlighter
+          searchWords={patterns.map((p) => new RegExp(p, 'gi'))}
+          textToHighlight={log}
+        />
+      </Pre>
     </TextWrap>
   )
 }

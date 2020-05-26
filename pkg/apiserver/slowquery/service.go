9127591a732160801e142c7abc1da274b0fda231
Breezewish <me@breeswish.org>
2020-05-26 13:46:26 +0800

    fix
    Signed-off-by: Breezewish <me@breeswish.org>

--- pkg/apiserver/slowquery/service.go
+++ pkg/apiserver/slowquery/service.go
@@ -17,6 +17,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pingcap/log"
+	"go.uber.org/zap"
 
 	"github.com/pingcap-incubator/tidb-dashboard/pkg/apiserver/user"
 	"github.com/pingcap-incubator/tidb-dashboard/pkg/apiserver/utils"
@@ -64,6 +66,9 @@ func (s *Service) listHandler(c *gin.Context) {
 		_ = c.Error(err)
 		return
 	}
+
+	log.Info("listHandler", zap.Any("results", results))
+
 	c.JSON(http.StatusOK, results)
 }
 

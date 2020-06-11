ec54dc386f26aa4ad207f64d89246ae022b7c3f6
Zheng Xiangsheng <hundundm@gmail.com>
2020-06-11 19:54:33 +0800

    add DefaultGRPCKeepaliveParams
    Signed-off-by: Zheng Xiangsheng <hundundm@gmail.com>

--- pkg/utils/grpc.go
+++ pkg/utils/grpc.go
@@ -18,10 +18,11 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/backoff"
+	"google.golang.org/grpc/keepalive"
 )
 
-var DefaultGRPCDialOptions = []grpc.DialOption{
-	grpc.WithConnectParams(grpc.ConnectParams{
+var (
+	DefaultGRPCConnectParams = grpc.ConnectParams{
 		Backoff: backoff.Config{
 			BaseDelay:  100 * time.Millisecond, // Default was 1 second
 			Multiplier: 1.6,                    // Default
@@ -29,5 +30,14 @@ var DefaultGRPCDialOptions = []grpc.DialOption{
 			MaxDelay:   3 * time.Second,        // Default was 120 seconds
 		},
 		MinConnectTimeout: 5 * time.Second, // Default was 20 seconds
-	}),
+	}
+	DefaultGRPCKeepaliveParams = keepalive.ClientParameters{
+		Time:                10 * time.Second,
+		Timeout:             3 * time.Second,
+		PermitWithoutStream: false,
+	}
+)
+
+var DefaultGRPCDialOptions = []grpc.DialOption{
+	grpc.WithConnectParams(DefaultGRPCConnectParams),
 }

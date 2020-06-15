dfbf8ec887bfb47ce881d5cb931b7ca2e0c52181
baurine <2008.hbl@gmail.com>
2020-06-15 16:24:49 +0800

    not use new js grammars and functions

--- ui/public/checkBrowser.js
+++ ui/public/checkBrowser.js
@@ -8,25 +8,17 @@ function checkBrowser() {
       text =
         'Your browser version is outdated, recommend to use the latest Chrome or Edge to get better experience.'
     }
-    const content = `
-      <div style="background: yellow;
-                  width: 100%;
-                  position: absolute;
-                  top: 0;
-                  z-index: 4;
-                  padding: 8px;
-                  text-align: center;
-                  transition: top 2s;">
-        <b>
-          ${text}
-          <a><span>X</span></a>
-        </b>
-      <div>
-    `
+
+    const content =
+      '<div style="background: yellow; width: 100%; position: absolute; top: 0; z-index: 4; padding: 8px; text-align: center; transition: top 2s;">' +
+      '<b>' +
+      text +
+      '<a><span>X</span></a></b><div>'
+
     var d = document.createElement('div')
     d.innerHTML = content
-    d.querySelector('a').onclick = function () {
-      d.querySelector('div').style.top = '-60px'
+    d.getElementsByTagName('a')[0].onclick = function () {
+      d.getElementsByTagName('div')[0].style.top = '-60px'
     }
     document.body.prepend(d)
   }

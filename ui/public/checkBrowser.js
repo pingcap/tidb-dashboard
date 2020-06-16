ad0826655043d3803afd6d8e2b15c61c68b86740
baurine <2008.hbl@gmail.com>
2020-06-16 12:50:10 +0800

    check browser language

--- ui/public/checkBrowser.js
+++ ui/public/checkBrowser.js
@@ -1,7 +1,19 @@
+function browserLang() {
+  // https://zzz.buzz/2016/01/13/detect-browser-language-in-javascript/
+  // https://social.msdn.microsoft.com/Forums/sqlserver/en-US/a3d78aaf-9f70-4826-954d-19183173c1c3/how-to-change-navigatoruserlanguage-in-ie11
+  return (
+    (navigator.languages && navigator.languages[0]) ||
+    navigator.language ||
+    navigator.browserLanguage ||
+    navigator.userLanguage ||
+    'en'
+  )
+}
+
 function checkBrowser() {
   if (!window.__supported_browsers__.test(navigator.userAgent)) {
     var text
-    if (navigator.language.indexOf('zh') === 0) {
+    if (browserLang().indexOf('zh') === 0) {
       text =
         '您的浏览器版本已过期，使用最新版本的 Chrome/Edge/Firefox/Safari 浏览器以便获得更好的体验。'
     } else {

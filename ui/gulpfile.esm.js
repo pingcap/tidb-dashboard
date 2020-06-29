f5ed429a6670097388b7e6611210f999a18ddac3
baurine <2008.hbl@gmail.com>
2020-06-29 14:42:31 +0800

    wip

--- ui/gulpfile.esm.js
+++ ui/gulpfile.esm.js
@@ -1,4 +1,4 @@
-import { task, watch, series, parallel } from 'gulp'
+import { task, watch, series, parallel, src, dest } from 'gulp'
 import shell from 'gulp-shell'
 
 task('swagger:generate_spec', shell.task('../scripts/generate_swagger_spec.sh'))
@@ -28,15 +28,6 @@ task(
   shell.task('yarn react-app-rewired build --library')
 )
 
-task(
-  'supportedBrowsers',
-  shell.task(
-    'sed "/^;checkBrowser(/d" ./public/checkBrowser.js > ./_tmp.js && ' +
-      'echo ";checkBrowser($(browserslist-useragent-regexp --allowHigherVersions))" >> ./_tmp.js && ' +
-      'mv ./_tmp.js ./public/checkBrowser.js'
-  )
-)
-
 task('build', series('swagger:generate', 'webpack:build'))
 
 task('build:library', series('swagger:generate', 'webpack:build:library'))
@@ -45,3 +36,38 @@ task(
   'dev',
   series('swagger:generate', parallel('swagger:watch', 'webpack:dev'))
 )
+
+/////////////////////////////////
+
+import Stream from 'stream'
+import { getUserAgentRegExp } from 'browserslist-useragent-regexp'
+
+// inspired from: https://github.com/brwnll/gulp-version-filename/blob/master/index.js
+function updateBrowserList() {
+  const stream = new Stream.Transform({ objectMode: true })
+
+  stream._transform = function (file, _filetype, callback) {
+    let oriContents = file.contents.toString()
+    const pattern = 'var __SUPPORTED_BROWSERS__ ='
+
+    if (oriContents.indexOf(pattern) < 0) {
+      return stream.emit('error', new Error(`Missing "${pattern}" pattern`))
+    }
+    const browserList = getUserAgentRegExp({ allowHigherVersions: true })
+    const regPattern = new RegExp(`${pattern} .+`)
+    const newContents = oriContents.replace(
+      regPattern,
+      `${pattern} ${browserList}`
+    )
+    file.contents = Buffer.from(newContents)
+    callback(null, file)
+  }
+
+  return stream
+}
+
+task('gen:browserlist', () => {
+  return src('public/checkBrowser.js')
+    .pipe(updateBrowserList())
+    .pipe(dest('public', { overwrite: true }))
+})

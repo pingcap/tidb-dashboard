87b8a777ba828f53465f1285ecde0a2b7cf0e32f
baurine <2008.hbl@gmail.com>
2020-06-30 14:58:40 +0800

    use useEventListener hook to simplify the code

--- ui/lib/components/TopLoadingBar/index.tsx
+++ ui/lib/components/TopLoadingBar/index.tsx
@@ -1,29 +1,15 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef } from 'react'
+import { useEventListener } from '@umijs/hooks'
 import LoadingBar from 'react-top-loading-bar'
 
 const useLoadingBar = () => {
-  const loadingBar = useRef<LoadingBar>(null)
-
-  useEffect(() => {
-    function startLoading() {
-      loadingBar?.current?.continuousStart()
-    }
-    window.addEventListener('single-spa:before-routing-event', startLoading)
-    return () =>
-      window.removeEventListener(
-        'single-spa:before-routing-event',
-        startLoading
-      )
-  }, [])
-
-  useEffect(() => {
-    function completeLoading() {
-      loadingBar?.current?.complete()
-    }
-    window.addEventListener('single-spa:routing-event', completeLoading)
-    return () =>
-      window.removeEventListener('single-spa:routing-event', completeLoading)
-  }, [])
+  const loadingBar = useRef<LoadingBar>()
+  useEventListener('single-spa:before-routing-event', () =>
+    loadingBar.current.continuousStart()
+  )
+  useEventListener('single-spa:routing-event', () =>
+    loadingBar.current.complete()
+  )
 
   return loadingBar
 }

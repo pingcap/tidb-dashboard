9d4982a563f6fd0382e9eb38e3508220e7e66192
Sparkle <1284531+baurine@users.noreply.github.com>
2020-06-30 11:06:58 +0800

    ui: check browser compatibility (#654)

--- ui/gulpfile.esm.js
+++ ui/gulpfile.esm.js
@@ -1,5 +1,7 @@
-import { task, watch, series, parallel } from 'gulp'
+import { task, watch, series, parallel, src, dest } from 'gulp'
 import shell from 'gulp-shell'
+import Stream from 'stream'
+import { getUserAgentRegExp } from 'browserslist-useragent-regexp'
 
 task('swagger:generate_spec', shell.task('../scripts/generate_swagger_spec.sh'))
 
@@ -36,3 +38,35 @@ task(
   'dev',
   series('swagger:generate', parallel('swagger:watch', 'webpack:dev'))
 )
+
+/////////////////////////////////
+
+// inspired from: https://github.com/brwnll/gulp-version-filename/blob/master/index.js
+function updateBrowserList() {
+  const stream = new Stream.Transform({ objectMode: true })
+
+  stream._transform = function (file, _filetype, callback) {
+    const oriContents = file.contents.toString()
+    const pattern = 'var __SUPPORTED_BROWSERS__ ='
+
+    if (oriContents.indexOf(pattern) < 0) {
+      return stream.emit('error', new Error(`Missing "${pattern}" pattern`))
+    }
+    const browserList = getUserAgentRegExp({ allowHigherVersions: true })
+    const regPattern = new RegExp(`${pattern} .+`)
+    const newContents = oriContents.replace(
+      regPattern,
+      `${pattern} ${browserList}`
+    )
+    file.contents = Buffer.from(newContents)
+    callback(null, file)
+  }
+
+  return stream
+}
+
+task('gen:browserlist', () => {
+  return src('public/checkBrowser.js')
+    .pipe(updateBrowserList())
+    .pipe(dest('public', { overwrite: true }))
+})

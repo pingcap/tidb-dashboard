392fbac08c76c8c47026cafa7c92149e5fac672d
Sparkle <1284531+baurine@users.noreply.github.com>
2020-07-01 13:35:30 +0800

    ui: show progress bar when switching pages (#661)

---
+++ ui/lib/components/TopLoadingBar/index.tsx
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react'
+import { useEventListener } from '@umijs/hooks'
+import LoadingBar from 'react-top-loading-bar'
+
+const useLoadingBar = () => {
+  const loadingBar = useRef<LoadingBar>()
+  useEventListener('single-spa:before-routing-event', () =>
+    loadingBar.current.continuousStart()
+  )
+  useEventListener('single-spa:routing-event', () =>
+    loadingBar.current.complete()
+  )
+
+  return loadingBar
+}
+
+export default function TopLoadinngBar() {
+  const loadingBar = useLoadingBar()
+  return <LoadingBar ref={loadingBar} />
+}

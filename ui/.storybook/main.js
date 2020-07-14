c35c97b82ded2c00cd2cf755c08a2dc9e4f9ad19
baurine <2008.hbl@gmail.com>
2020-07-14 14:28:58 +0800

    npx -p @storybook/cli sb init --story-format=csf-ts
    - https://gist.github.com/shilman/bc9cbedb2a7efb5ec6710337cbd20c0c

---
+++ ui/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+}

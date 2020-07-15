d6b4d376bd21ba9d93cb8fa92979722fd2096ea3
baurine <2008.hbl@gmail.com>
2020-07-15 11:31:41 +0800

    modify storybook webpack config to pass compile

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -1,8 +1,32 @@
+const path = require('path')
+
+function includeMorePaths(config) {
+  // fine rule to handle *.tsx files
+  for (const rule of config.module.rules) {
+    for (const subRule of rule.oneOf || []) {
+      // /\.(js|mjs|jsx|ts|tsx)$/
+      if (subRule.test instanceof RegExp && subRule.test.test('.tsx')) {
+        libFolder = path.resolve(__dirname, '../lib')
+        subRule.include.push(libFolder)
+        break
+      }
+    }
+  }
+
+  return config
+}
+
+function addMoreAlias(config) {
+  config.resolve.alias['@lib'] = path.resolve(__dirname, '../lib')
+  return config
+}
+
 module.exports = {
-  stories: ['../lib/components/**/*.stories.(ts|tsx|js|jsx)'],
+  stories: ['../lib/components/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  webpackFinal: (config) => addMoreAlias(includeMorePaths(config)),
 }

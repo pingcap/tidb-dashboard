eecd719b70459127753168a00902797431df52bc
baurine <2008.hbl@gmail.com>
2020-07-16 15:51:02 +0800

    add stories for BaseSelect

---
+++ ui/lib/components/BaseSelect/index.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from 'antd'
+import BaseSelect from './'
+
+export default {
+  title: 'Base Select',
+}
+
+export const shortContent = () => (
+  <BaseSelect
+    dropdownRender={() => <div>Content</div>}
+    valueRender={() => <span>Short</span>}
+  />
+)
+
+export const longContent = () => (
+  <BaseSelect
+    style={{ width: 120 }}
+    dropdownRender={() => <div>Content</div>}
+    valueRender={() => <span>Very Lonnnnnnnnng Value</span>}
+  />
+)
+
+export const disabledContent = () => (
+  <BaseSelect
+    disabled
+    dropdownRender={() => <div>Content</div>}
+    valueRender={() => <span>Disabled</span>}
+  />
+)
+
+export const antdButton = () => <Button>Antd Button</Button>

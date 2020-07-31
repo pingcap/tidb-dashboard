939200d9ac6bc1e3c09d3fc546f874e8434347c7
baurine <2008.hbl@gmail.com>
2020-07-31 15:48:01 +0800

    refine by useClientRequest

--- ui/lib/apps/Diagnose/components/DiagnosisTable.tsx
+++ ui/lib/apps/Diagnose/components/DiagnosisTable.tsx
@@ -1,15 +1,17 @@
-import { LoadingOutlined } from '@ant-design/icons'
-import { Button, message } from 'antd'
-import React, { useEffect, useState, useMemo } from 'react'
+import { Button } from 'antd'
+import { AxiosPromise, CancelToken } from 'axios'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { LoadingOutlined } from '@ant-design/icons'
 
+import client, { DiagnoseTableDef } from '@lib/client'
 import { CardTable, DateTime } from '@lib/components'
-import client from '@lib/client'
+import { useClientRequest } from '@lib/utils/useClientRequest'
 
 import { diagnosisColumns } from '../utils/tableColumns'
 
 // FIXME: use better naming
-// stableTimeRange: used to start diagnosing when triggering by clicking "Start All" outside this component
+// stableTimeRange: used to start diagnosing when triggering by clicking "Start" outside this component
 // unstableTimeRange: used to start diagnosing when triggering by clicking "Start" inside this component
 export interface IDiagnosisTableProps {
   stableTimeRange: [number, number]
@@ -17,12 +19,15 @@ export interface IDiagnosisTableProps {
   kind: string
 }
 
+type ReqFnType = (cancel: CancelToken) => AxiosPromise<DiagnoseTableDef>
+
 export default function DiagnosisTable({
   stableTimeRange,
   unstableTimeRange,
   kind,
 }: IDiagnosisTableProps) {
   const { t } = useTranslation()
+
   const [internalTimeRange, setInternalTimeRange] = useState<[number, number]>([
     0,
     0,
@@ -38,47 +43,49 @@ export default function DiagnosisTable({
     [internalTimeRange, unstableTimeRange]
   )
 
-  const [items, setItems] = useState<any[]>([])
-  const columns = useMemo(() => diagnosisColumns(items), [items])
-  const [loading, setLoading] = useState(false)
-
+  const reqFn = useRef<ReqFnType | null>(null)
   useEffect(() => {
-    async function getData() {
-      if (internalTimeRange[0] === 0 || internalTimeRange[1] === 0) {
-        return
-      }
-      setLoading(true)
-      try {
-        const res = await client.getInstance().diagnoseDiagnosisPost({
+    reqFn.current = (cancelToken) =>
+      client.getInstance().diagnoseDiagnosisPost(
+        {
           start_time: internalTimeRange[0],
           end_time: internalTimeRange[1],
           kind,
-        })
-        const _columns =
-          res?.data?.column?.map((col) => col.toLocaleLowerCase()) || []
-        const _items: any[] =
-          res?.data?.rows?.map((row) => {
-            let obj = {}
-            row.values?.forEach((v, idx) => {
-              const key = _columns[idx]
-              obj[key] = v
-            })
-            return obj
-          }) || []
-        setItems(_items)
-      } catch (error) {
-        message.error(error.message)
-      }
-      setLoading(false)
-    }
-    getData()
+        },
+        { cancelToken }
+      )
   }, [internalTimeRange, kind])
 
+  const { data, isLoading, error, sendRequest } = useClientRequest(
+    (cancelToken) => reqFn.current!(cancelToken),
+    { immediate: false }
+  )
+
+  useEffect(() => {
+    if (internalTimeRange[0] !== 0) {
+      sendRequest()
+    }
+  }, [internalTimeRange])
+
+  const [items, columns] = useMemo(() => {
+    const _columns = data?.column?.map((col) => col.toLocaleLowerCase()) || []
+    const _items: any[] =
+      data?.rows?.map((row) => {
+        let obj = {}
+        row.values?.forEach((v, idx) => {
+          const key = _columns[idx]
+          obj[key] = v
+        })
+        return obj
+      }) || []
+    return [_items, diagnosisColumns(_items)]
+  }, [data])
+
   function cardExtra() {
-    if (loading) {
+    if (isLoading) {
       return <LoadingOutlined />
     }
-    if (timeChanged) {
+    if (timeChanged || error) {
       return <Button onClick={handleStart}>Start</Button>
     }
     return null
@@ -97,6 +104,7 @@ export default function DiagnosisTable({
         )
       }
       cardExtra={cardExtra()}
+      errors={[error]}
       columns={columns}
       items={items}
       extendLastColumn

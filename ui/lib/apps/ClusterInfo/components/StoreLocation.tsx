f1bad516756e338b0f7010a573b5fc88eec65675
baurine <2008.hbl@gmail.com>
2020-08-05 11:11:52 +0800

    wip

--- ui/lib/apps/ClusterInfo/components/StoreLocation.tsx
+++ ui/lib/apps/ClusterInfo/components/StoreLocation.tsx
@@ -5,21 +5,32 @@ import { Pre } from '@lib/components'
 
 type TreeNode = {
   name: string
-  children?: TreeNode[]
-  value?: any
+  children: TreeNode[]
 }
 
 function buildTopology(data: TopologyStoreLocation | undefined) {
-  let treeData: TreeNode = { name: '' }
+  let treeData: TreeNode = { name: '', children: [] }
   if ((data?.location_labels?.length || 0) > 0) {
     const locationLabels: string[] = data?.location_labels?.split(',') || []
     treeData.name = locationLabels[0]
-    treeData.children = []
 
     for (const store of data?.stores || []) {
       let curNode = treeData
       for (const curLabel of locationLabels) {
+        const curLabelVal = store.labels![curLabel]
+        if (curLabelVal === undefined) {
+          continue
+        }
+        let subNode: TreeNode | undefined = curNode.children.find(
+          (el) => el.name === curLabelVal
+        )
+        if (subNode === undefined) {
+          subNode = { name: curLabelVal, children: [] }
+          curNode.children.push(subNode)
+        }
+        curNode = subNode
       }
+      curNode.children.push({ name: store.address!, children: [] })
     }
   }
   return treeData

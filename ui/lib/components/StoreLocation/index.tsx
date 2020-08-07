8bf14f8fe0270b36de2751c4337b70972ae9bd84
root <1439862520@qq.com>
2020-08-07 12:44:50 +0800

    StoreLocation-Tree

--- ui/lib/components/StoreLocation/index.tsx
+++ ui/lib/components/StoreLocation/index.tsx
@@ -26,13 +26,15 @@ export default function StoreLocation({
 }: IStoreLocationProps) {
   const opt = {
     tooltip: {
-      trigger: 'item', 
-      triggerOn: 'mousemove', 
+      //提示框组件
+      trigger: 'item', //触发类型，默认：item（数据项图形触发，主要在散点图，饼图等无类目轴的图表中使用）。可选：'axis'：坐标轴触发，主要在柱状图，折线图等会使用类目轴的图表中使用。'none':什么都不触发。
+      triggerOn: 'mousemove', //提示框触发的条件，默认mousemove|click（鼠标点击和移动时触发）。可选mousemove：鼠标移动时，click：鼠标点击时，none：
     },
     series: [
+      //系列列表
       {
         type: 'tree',
-        
+
         data: [dataSource],
 
         top: '1%',
@@ -43,15 +45,17 @@ export default function StoreLocation({
         symbolSize: 7,
 
         label: {
+          //每个节点所对应的标签的样式
           normal: {
-            position: 'left',
-            verticalAlign: 'middle',
-            align: 'right',
-            fontSize: 9, 
+            position: 'left', //标签的位置
+            verticalAlign: 'middle', //文字垂直对齐方式，默认自动。可选：top，middle，bottom
+            align: 'right', //文字水平对齐方式，默认自动。可选：top，center，bottom
+            fontSize: 9, //标签文字大小
           },
         },
 
         leaves: {
+          //叶子节点的特殊配置
           label: {
             normal: {
               position: 'right',
@@ -61,9 +65,9 @@ export default function StoreLocation({
           },
         },
 
-        expandAndCollapse: true, 
-        animationDuration: 550, 
-        animationDurationUpdate: 750, 
+        expandAndCollapse: true, //子树折叠和展开的交互，默认打开
+        animationDuration: 550, //初始动画的时长，支持回调函数,默认1000
+        animationDurationUpdate: 750, //数据更新动画的时长，默认300
       },
     ],
   }

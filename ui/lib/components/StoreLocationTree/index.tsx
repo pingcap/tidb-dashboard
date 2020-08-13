deb654874cab4eac19b33c9980ec899208f37fde
baurine <2008.hbl@gmail.com>
2020-08-13 23:37:44 +0800

    refine

--- ui/lib/components/StoreLocationTree/index.tsx
+++ ui/lib/components/StoreLocationTree/index.tsx
@@ -5,9 +5,9 @@ export interface IStoreLocationProps {
   dataSource: any
 }
 
-const margin = { top: 10, right: 120, bottom: 10, left: 40 }
+const margin = { top: 40, right: 120, bottom: 10, left: 80 }
 const width = 954
-const dx = 10
+const dx = 40
 const dy = width / 6
 
 const tree = d3.tree().nodeSize([dx, dy])
@@ -35,7 +35,7 @@ export default function StoreLocationTree({ dataSource }: IStoreLocationProps) {
     svg.selectAll('g').remove()
     svg
       .attr('viewBox', [-margin.left, -margin.top, width, dx] as any)
-      .style('font', '10px sans-serif')
+      .style('font', '16px sans-serif')
       .style('user-select', 'none')
 
     const gLink = svg
@@ -43,7 +43,7 @@ export default function StoreLocationTree({ dataSource }: IStoreLocationProps) {
       .attr('fill', 'none')
       .attr('stroke', '#555')
       .attr('stroke-opacity', 0.4)
-      .attr('stroke-width', 1.5)
+      .attr('stroke-width', 2)
 
     const gNode = svg
       .append('g')
@@ -55,7 +55,8 @@ export default function StoreLocationTree({ dataSource }: IStoreLocationProps) {
       const nodes = root.descendants().reverse()
       const links = root.links()
 
-      // Compute the new tree layout.
+      // compute the new tree layout
+      // it modifies root self
       tree(root)
 
       let left = root
@@ -78,14 +79,14 @@ export default function StoreLocationTree({ dataSource }: IStoreLocationProps) {
         ] as any)
         .tween('resize', () => () => svg.dispatch('toggle'))
 
-      // Update the nodes…
+      // update the nodes
       const node = gNode.selectAll('g').data(nodes, (d: any) => d.id)
 
-      // Enter any new nodes at the parent's previous position.
+      // enter any new nodes at the parent's previous position
       const nodeEnter = node
         .enter()
         .append('g')
-        .attr('transform', (d) => `translate(${source.y0},${source.x0})`)
+        .attr('transform', (_d) => `translate(${source.y0},${source.x0})`)
         .attr('fill-opacity', 0)
         .attr('stroke-opacity', 0)
         .on('click', (d: any) => {
@@ -95,32 +96,37 @@ export default function StoreLocationTree({ dataSource }: IStoreLocationProps) {
 
       nodeEnter
         .append('circle')
-        .attr('r', 2.5)
+        .attr('r', 5)
         .attr('fill', (d: any) => (d._children ? '#555' : '#999'))
         .attr('stroke-width', 10)
 
       nodeEnter
         .append('text')
         .attr('dy', '0.31em')
-        .attr('x', (d: any) => (d._children ? -6 : 6))
+        .attr('x', (d: any) => (d._children ? -8 : 8))
         .attr('text-anchor', (d: any) => (d._children ? 'end' : 'start'))
-        .text((d: any) => d.data.name)
+        .text(({ data: { name, value } }: any) => {
+          if (value) {
+            return `${name}: ${value}`
+          }
+          return name
+        })
         .clone(true)
         .lower()
         .attr('stroke-linejoin', 'round')
         .attr('stroke-width', 3)
         .attr('stroke', 'white')
 
-      // Transition nodes to their new position.
-      const nodeUpdate = node
+      // transition nodes to their new position
+      node
         .merge(nodeEnter as any)
         .transition(transition as any)
         .attr('transform', (d: any) => `translate(${d.y},${d.x})`)
         .attr('fill-opacity', 1)
         .attr('stroke-opacity', 1)
 
-      // Transition exiting nodes to the parent's new position.
-      const nodeExit = node
+      // transition exiting nodes to the parent's new position
+      node
         .exit()
         .transition(transition as any)
         .remove()
@@ -128,35 +134,35 @@ export default function StoreLocationTree({ dataSource }: IStoreLocationProps) {
         .attr('fill-opacity', 0)
         .attr('stroke-opacity', 0)
 
-      // Update the links…
+      // update the links
       const link = gLink.selectAll('path').data(links, (d: any) => d.target.id)
 
-      // Enter any new links at the parent's previous position.
+      // enter any new links at the parent's previous position
       const linkEnter = link
         .enter()
         .append('path')
-        .attr('d', (d) => {
+        .attr('d', (_d) => {
           const o = { x: source.x0, y: source.y0 }
           return diagonal({ source: o, target: o } as any)
         })
 
-      // Transition links to their new position.
+      // transition links to their new position
       link
         .merge(linkEnter as any)
         .transition(transition as any)
         .attr('d', diagonal as any)
 
-      // Transition exiting nodes to the parent's new position.
+      // transition exiting nodes to the parent's new position
       link
         .exit()
         .transition(transition as any)
         .remove()
-        .attr('d', (d) => {
+        .attr('d', (_d) => {
           const o = { x: source.x, y: source.y }
           return diagonal({ source: o, target: o } as any)
         })
 
-      // Stash the old positions for transition.
+      // ttash the old positions for transition
       root.eachBefore((d) => {
         d.x0 = d.x
         d.y0 = d.y

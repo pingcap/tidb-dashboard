024a13ba566a29f07dcc058fe72659d323e68372
baurine <2008.hbl@gmail.com>
2020-08-13 23:45:44 +0800

    remove test code

--- ui/lib/components/D3/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Circle, Circles } from '.'
-
-export default {
-  title: 'D3 Circle',
-}
-
-export const circle = () => <Circle />
-export const circles = () => <Circles />

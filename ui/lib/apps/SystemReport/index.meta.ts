600cf5b8d494c0a5ebaf90f067e2d175ef3c270f
baurine <2008.hbl@gmail.com>
2020-08-18 10:31:44 +0800

    i18n wip

--- ui/lib/apps/SystemReport/index.meta.ts
+++ ui/lib/apps/SystemReport/index.meta.ts
@@ -1,9 +1,9 @@
-import { SafetyCertificateOutlined } from '@ant-design/icons'
+import { SnippetsOutlined } from '@ant-design/icons'
 
 export default {
   id: 'system_report',
   routerPrefix: '/system_report',
-  icon: SafetyCertificateOutlined,
+  icon: SnippetsOutlined,
   translations: require.context('./translations/', false, /\.yaml$/),
   reactRoot: () => import(/* webpackChunkName: "app_system_report" */ '.'),
 }

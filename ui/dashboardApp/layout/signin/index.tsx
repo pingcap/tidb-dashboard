00d696d034c019bb5867cc9470b9630225080abc
Breezewish <me@breeswish.org>
2020-08-21 16:36:23 +0800

    Fix animations

--- ui/dashboardApp/layout/signin/index.tsx
+++ ui/dashboardApp/layout/signin/index.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames'
 import * as singleSpa from 'single-spa'
 import { Root } from '@lib/components'
 import React, { useState, useEffect, useRef } from 'react'
@@ -8,7 +9,6 @@ import {
   UserOutlined,
 } from '@ant-design/icons'
 import { Form, Input, Button, message } from 'antd'
-import { motion } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
 import LanguageDropdown from '@lib/components/LanguageDropdown'
 import client from '@lib/client'
@@ -16,17 +16,6 @@ import * as auth from '@lib/utils/auth'
 
 import { ReactComponent as Logo } from './logo.svg'
 import styles from './index.module.less'
-
-const AnimationItem = (props) => {
-  return (
-    <motion.div
-      variants={{ open: { y: 0, opacity: 1 }, initial: { y: 50, opacity: 0 } }}
-      transition={{ ease: 'easeOut' }}
-    >
-      {props.children}
-    </motion.div>
-  )
-}
 
 function TiDBSignInForm({ registry }) {
   const { t } = useTranslation()
@@ -83,88 +72,69 @@ function TiDBSignInForm({ registry }) {
 
   return (
     <Form
+      className="formAnimation"
       name="tidb_signin"
       onFinish={handleSubmit}
       layout="vertical"
       initialValues={{ username: 'root' }}
       form={refForm}
     >
-      <motion.div
-        initial="initial"
-        animate="open"
-        variants={{
-          open: { transition: { staggerChildren: 0.03, delayChildren: 0.5 } },
-        }}
+      <Logo className={styles.logo} />
+      <Form.Item>
+        <h2>{t('signin.form.tidb_auth.title')}</h2>
+      </Form.Item>
+      <Form.Item
+        name="username"
+        label={t('signin.form.username')}
+        rules={[
+          {
+            required: true,
+            message: t('signin.form.tidb_auth.check.username'),
+          },
+        ]}
       >
-        <AnimationItem>
-          <Logo className={styles.logo} />
-        </AnimationItem>
-        <AnimationItem>
-          <Form.Item>
-            <h2>{t('signin.form.tidb_auth.title')}</h2>
-          </Form.Item>
-        </AnimationItem>
-        <AnimationItem>
-          <Form.Item
-            name="username"
-            label={t('signin.form.username')}
-            rules={[
-              {
-                required: true,
-                message: t('signin.form.tidb_auth.check.username'),
-              },
-            ]}
-          >
-            <Input
-              onInput={clearErrorMessages}
-              prefix={<UserOutlined />}
-              disabled
-            />
-          </Form.Item>
-        </AnimationItem>
-        <AnimationItem>
-          <Form.Item
-            name="password"
-            label={t('signin.form.password')}
-            {...(signInError && {
-              help: signInError,
-              validateStatus: 'error',
-            })}
-          >
-            <Input
-              prefix={<LockOutlined />}
-              type="password"
-              disabled={loading}
-              onInput={clearErrorMessages}
-              ref={refPassword}
-            />
-          </Form.Item>
-        </AnimationItem>
-        <AnimationItem>
-          <Form.Item>
-            <Button
-              id="signin_btn"
-              type="primary"
-              htmlType="submit"
-              size="large"
-              loading={loading}
-              className={styles.signInButton}
-              block
-            >
-              {t('signin.form.button')}
-            </Button>
-          </Form.Item>
-        </AnimationItem>
-        <AnimationItem>
-          <div className={styles.extraLink}>
-            <LanguageDropdown>
-              <a>
-                <GlobalOutlined /> Switch Language <DownOutlined />
-              </a>
-            </LanguageDropdown>
-          </div>
-        </AnimationItem>
-      </motion.div>
+        <Input
+          onInput={clearErrorMessages}
+          prefix={<UserOutlined />}
+          disabled
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label={t('signin.form.password')}
+        {...(signInError && {
+          help: signInError,
+          validateStatus: 'error',
+        })}
+      >
+        <Input
+          prefix={<LockOutlined />}
+          type="password"
+          disabled={loading}
+          onInput={clearErrorMessages}
+          ref={refPassword}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button
+          id="signin_btn"
+          type="primary"
+          htmlType="submit"
+          size="large"
+          loading={loading}
+          className={styles.signInButton}
+          block
+        >
+          {t('signin.form.button')}
+        </Button>
+      </Form.Item>
+      <div className={styles.extraLink}>
+        <LanguageDropdown>
+          <a>
+            <GlobalOutlined /> Switch Language <DownOutlined />
+          </a>
+        </LanguageDropdown>
+      </div>
     </Form>
   )
 }
@@ -178,12 +148,7 @@ function App({ registry }) {
             <TiDBSignInForm registry={registry} />
           </div>
         </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ ease: 'easeOut', duration: 0.5 }}
-          className={styles.landing}
-        ></motion.div>
+        <div className={cx(styles.landing, 'landingAnimation')}></div>
       </div>
     </Root>
   )
--- ui/lib/components/AnimatedSkeleton/index.tsx
+++ ui/lib/components/AnimatedSkeleton/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import cx from 'classnames'
 import { Skeleton } from 'antd'
 import { SkeletonProps } from 'antd/lib/skeleton'
-import { animated, useSpring } from 'react-spring'
 
 import styles from './index.module.less'
 
@@ -11,62 +10,28 @@ export interface IAnimatedSkeletonProps extends SkeletonProps {
   children?: React.ReactNode
 }
 
-function opacityToDisplay(v) {
-  return v === 0 ? 'none' : 'block'
-}
-
-function getTargetProps(showSkeleton) {
-  return {
-    skeletonOpacity: showSkeleton ? 1 : 0,
-    contentOpacity: showSkeleton ? 0 : 1,
-  }
-}
-
 function AnimatedSkeleton({
   showSkeleton,
   children,
   ...restProps
 }: IAnimatedSkeletonProps) {
-  const skeletonRef = useRef<HTMLDivElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
-  const [isAnimating, setAnimating] = useState(false)
-  const [minHeight, setMinHeight] = useState<string | number>('auto')
-
-  const [props, setProps] = useSpring(() => ({
-    ...getTargetProps(showSkeleton),
-    onStart: () => {
-      setAnimating(true)
-      if (!skeletonRef.current || !contentRef.current) {
-        return
-      }
-      let minHeight = 0
-      minHeight = Math.max(minHeight, skeletonRef.current!.offsetHeight)
-      minHeight = Math.max(minHeight, contentRef.current!.offsetHeight)
-      setMinHeight(minHeight)
-    },
-    onRest: () => {
-      setAnimating(false)
-      setMinHeight('auto')
-    },
-  }))
+  const [skeletonAppears, setSkeletonAppears] = useState(0)
 
   useEffect(() => {
-    setProps(getTargetProps(showSkeleton))
-  }, [showSkeleton, setProps])
+    if (showSkeleton) {
+      setSkeletonAppears((v) => v + 1)
+    }
+  }, [showSkeleton])
 
   return (
-    <div
-      className={cx(styles.container, { [styles.isAnimating]: isAnimating })}
-      style={{ minHeight: minHeight }}
-    >
-      <animated.div
-        className={styles.skeleton}
-        style={{
-          opacity: props.skeletonOpacity,
-          display: props.skeletonOpacity.interpolate(opacityToDisplay),
-        }}
-      >
-        <div ref={skeletonRef}>
+    <div className={cx(styles.container)}>
+      {showSkeleton && (
+        <div
+          className={cx({
+            skeletonAnimationFirstTime: skeletonAppears === 1,
+            skeletonAnimationNotFirstTime: skeletonAppears > 1,
+          })}
+        >
           <Skeleton
             active
             title={false}
@@ -74,17 +39,10 @@ function AnimatedSkeleton({
             {...restProps}
           />
         </div>
-      </animated.div>
-      <animated.div
-        className={styles.content}
-        style={{
-          opacity: props.contentOpacity,
-        }}
-      >
-        <div ref={contentRef}>{children}</div>
-      </animated.div>
+      )}
+      {!showSkeleton && <div className="contentAnimation">{children}</div>}
     </div>
   )
 }
 
-export default AnimatedSkeleton
+export default React.memo(AnimatedSkeleton)

356a6405614cab033db4abc8080491b3dba346b0
baurine <2008.hbl@gmail.com>
2020-09-08 16:15:34 +0800

    wip

--- scripts/wait_tiup_playground.sh
+++ scripts/wait_tiup_playground.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 # Wait unitl `tiup playground` command run success
 
-source /home/runner/.profile
-
 INTERVAL=$1
 MAX_TIMES=$2
 
-if [ $INTERVAL -eq "" || ${MAX_TIMES} -eq "" ]
-then
+echo $INTERVAL
+echo ${MAX_TIMES}
+
+if ([ -z "${INTERVAL}" ] || [ -z "${MAX_TIMES}" ]); then
   echo "Usage: command <interval> <max_times>"
   exit 1
 fi
 
-for ((i=0; i<${MAX_TIMES}; i++))
-do
+for ((i=0; i<${MAX_TIMES}; i++)); do
   tiup playground display
-  if [ $? -eq 0 ]
-  then
+  if [ $? -eq 0 ]; then
     exit 0
   fi
   sleep ${INTERVAL}

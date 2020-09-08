fc0c80cab8cd4187ae01f59e1dfe37b77d03b75c
baurine <2008.hbl@gmail.com>
2020-09-08 15:50:29 +0800

    wip

--- scripts/wait_tiup_playground.sh
+++ scripts/wait_tiup_playground.sh
@@ -6,6 +6,12 @@ source /home/runner/.profile
 INTERVAL=$1
 MAX_TIMES=$2
 
+if [ $INTERVAL -eq "" || ${MAX_TIMES} -eq "" ]
+then
+  echo "Usage: command <interval> <max_times>"
+  exit 1
+fi
+
 for ((i=0; i<${MAX_TIMES}; i++))
 do
   tiup playground display

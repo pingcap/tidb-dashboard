8ca617a34bd868a9b020765ec7dac0a8b3f74b8b
Wenxuan <hi@breeswish.org>
2021-05-12 13:31:53 +0800

    ui: Improve settings description for Statement (#920)

--- ui/lib/apps/Statement/pages/List/StatementSettingForm.tsx
+++ ui/lib/apps/Statement/pages/List/StatementSettingForm.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import {
   Form,
-  InputNumber,
   Skeleton,
   Switch,
   Input,
@@ -21,185 +20,167 @@ interface Props {
   onConfigUpdated: () => any
 }
 
-type InternalStatementConfig = StatementEditableConfig & {
-  max_refresh_interval: number
-  max_history_size: number
-}
-
 const convertArrToObj = (arr: number[]) =>
   arr.reduce((acc, cur) => {
     acc[cur] = cur
     return acc
   }, {})
 
-const REFRESH_INTERVAL_MARKS = convertArrToObj([1, 5, 15, 30, 60])
-const HISTORY_SIZE_MARKS = convertArrToObj([1, 64, 128, 192, 255])
-
 function StatementSettingForm({ onClose, onConfigUpdated }: Props) {
   const [submitting, setSubmitting] = useState(false)
   const { t } = useTranslation()
 
-  const [curRefreshInterval, setCurRefreshInterval] = useState(0)
-  const [curHistorySize, setCurHistorySize] = useState(0)
-  const dataRetainDuration = useMemo(() => {
-    const totalMins = curRefreshInterval * curHistorySize
-    const day = Math.floor(totalMins / (24 * 60))
-    const hour = Math.floor((totalMins - day * 24 * 60) / 60)
-    const min = totalMins - day * 24 * 60 - hour * 60
-    return `${day} day ${hour} hour ${min} min`
-  }, [curRefreshInterval, curHistorySize])
-
   const {
-    data: oriConfig,
+    data: initialConfig,
     isLoading: loading,
     error,
   } = useClientRequest((reqConfig) =>
     client.getInstance().statementsConfigGet(reqConfig)
   )
 
-  const config = useMemo(() => {
-    if (oriConfig) {
-      const refresh_interval = Math.ceil(oriConfig.refresh_interval! / 60)
-      const max_refresh_interval = Math.max(refresh_interval, 60)
-      const max_history_size = Math.max(oriConfig.history_size!, 255)
+  const handleSubmit = useCallback(
+    (values) => {
+      async function updateConfig(values) {
+        const newConfig: StatementEditableConfig = {
+          enable: values.enable,
+          max_size: values.max_size,
+          refresh_interval: values.refresh_interval * 60,
+          history_size: values.history_size,
+          internal_query: values.internal_query,
+        }
+        try {
+          setSubmitting(true)
+          await client.getInstance().statementsConfigPost(newConfig)
+          onClose()
+          onConfigUpdated()
+        } finally {
+          setSubmitting(false)
+        }
+      }
 
-      return {
-        ...oriConfig,
-        refresh_interval,
-        max_refresh_interval,
-        max_history_size,
-      } as InternalStatementConfig
-    }
-    return null
-  }, [oriConfig])
-
-  useEffect(() => {
-    if (config) {
-      setCurRefreshInterval(config.refresh_interval!)
-      setCurHistorySize(config.history_size!)
-    }
-  }, [config])
-
-  async function updateConfig(values) {
-    const newConfig: StatementEditableConfig = {
-      enable: values.enable,
-      max_size: values.max_size,
-      refresh_interval: values.refresh_interval * 60,
-      history_size: values.history_size,
-    }
-    try {
-      setSubmitting(true)
-      await client.getInstance().statementsConfigPost(newConfig)
-      onClose()
-      onConfigUpdated()
-    } finally {
-      setSubmitting(false)
-    }
-  }
-
-  function handleSubmit(values) {
-    if (oriConfig?.enable && !values.enable) {
-      // warning
-      Modal.confirm({
-        title: t('statement.settings.close_statement'),
-        icon: <ExclamationCircleOutlined />,
-        content: t('statement.settings.close_statement_warning'),
-        okText: t('statement.settings.actions.close'),
-        cancelText: t('statement.settings.actions.cancel'),
-        okButtonProps: { danger: true },
-        onOk: () => updateConfig(values),
-      })
-    } else {
-      updateConfig(values)
-    }
-  }
+      if (!values.enable) {
+        // warning
+        Modal.confirm({
+          title: t('statement.settings.close_statement'),
+          icon: <ExclamationCircleOutlined />,
+          content: t('statement.settings.close_statement_warning'),
+          okText: t('statement.settings.actions.close'),
+          cancelText: t('statement.settings.actions.cancel'),
+          okButtonProps: { danger: true },
+          onOk: () => updateConfig(values),
+        })
+      } else {
+        updateConfig(values)
+      }
+    },
+    [t, onClose, onConfigUpdated]
+  )
 
   return (
     <>
       {error && <ErrorBar errors={[error]} />}
       {loading && <Skeleton active={true} paragraph={{ rows: 5 }} />}
-      {!loading && config && (
-        <Form layout="vertical" initialValues={config} onFinish={handleSubmit}>
+      {!loading && initialConfig && (
+        <Form
+          layout="vertical"
+          initialValues={{
+            ...initialConfig,
+            refresh_interval: Math.floor(
+              (initialConfig.refresh_interval ?? 0) / 60
+            ),
+          }}
+          onFinish={handleSubmit}
+        >
           <Form.Item
-            name="enable"
             valuePropName="checked"
             label={t('statement.settings.switch')}
+            extra={t('statement.settings.switch_tooltip')}
           >
-            <Switch />
+            <Form.Item noStyle name="enable" valuePropName="checked">
+              <Switch />
+            </Form.Item>
           </Form.Item>
           <Form.Item
             noStyle
             shouldUpdate={(prev, cur) => prev.enable !== cur.enable}
           >
-            {({ getFieldValue }) => {
-              return (
-                getFieldValue('enable') && (
-                  <Form.Item noStyle>
-                    <Form.Item
-                      name="max_size"
-                      label={t('statement.settings.max_size')}
-                    >
-                      <InputNumber min={1} />
-                    </Form.Item>
-                    <Form.Item label={t('statement.settings.refresh_interval')}>
-                      <Input.Group>
-                        <Form.Item noStyle name="refresh_interval">
-                          <InputNumber
-                            min={1}
-                            max={config.max_refresh_interval}
-                            formatter={(value) => `${value} min`}
-                            parser={(value) =>
-                              value?.replace(/[^\d]/g, '') || ''
-                            }
-                            onChange={(val) =>
-                              setCurRefreshInterval(val as number)
-                            }
-                          />
-                        </Form.Item>
-                        <Form.Item noStyle name="refresh_interval">
-                          <Slider
-                            min={1}
-                            max={config.max_refresh_interval}
-                            marks={{
-                              ...REFRESH_INTERVAL_MARKS,
-                              [config.max_refresh_interval]: `${config.max_refresh_interval}`,
-                            }}
-                            onChange={(val) => setCurRefreshInterval(val)}
-                          />
-                        </Form.Item>
-                      </Input.Group>
-                    </Form.Item>
-                    <Form.Item label={t('statement.settings.history_size')}>
-                      <Input.Group>
-                        <Form.Item noStyle name="history_size">
-                          <InputNumber
-                            min={1}
-                            max={config.max_history_size}
-                            onChange={(val) => setCurHistorySize(val as number)}
-                          />
-                        </Form.Item>
-                        <Form.Item noStyle name="history_size">
-                          <Slider
-                            min={1}
-                            max={config.max_history_size}
-                            marks={{
-                              ...HISTORY_SIZE_MARKS,
-                              [config.max_history_size]: `${config.max_history_size}`,
-                            }}
-                            onChange={(val) => setCurHistorySize(val)}
-                          />
-                        </Form.Item>
-                      </Input.Group>
-                    </Form.Item>
-                    <Form.Item label={t('statement.settings.keep_duration')}>
-                      <span style={{ color: '#555' }}>
-                        {dataRetainDuration}
-                      </span>
-                    </Form.Item>
+            {({ getFieldValue }) =>
+              getFieldValue('enable') && (
+                <>
+                  <Form.Item
+                    label={t('statement.settings.max_size')}
+                    extra={t('statement.settings.max_size_tooltip')}
+                  >
+                    <Input.Group>
+                      <Form.Item noStyle name="max_size">
+                        <Slider
+                          min={0}
+                          max={5000}
+                          step={100}
+                          marks={convertArrToObj([200, 1000, 2000, 5000])}
+                        />
+                      </Form.Item>
+                    </Input.Group>
                   </Form.Item>
-                )
+                  <Form.Item
+                    label={t('statement.settings.refresh_interval')}
+                    extra={t('statement.settings.refresh_interval_tooltip')}
+                  >
+                    <Input.Group>
+                      <Form.Item noStyle name="refresh_interval">
+                        <Slider
+                          min={1}
+                          max={60}
+                          step={null}
+                          marks={convertArrToObj([1, 5, 15, 30, 60])}
+                        />
+                      </Form.Item>
+                    </Input.Group>
+                  </Form.Item>
+                  <Form.Item
+                    label={t('statement.settings.history_size')}
+                    extra={t('statement.settings.history_size_tooltip')}
+                  >
+                    <Input.Group>
+                      <Form.Item noStyle name="history_size">
+                        <Slider
+                          min={1}
+                          max={255}
+                          marks={convertArrToObj([1, 255])}
+                        />
+                      </Form.Item>
+                    </Input.Group>
+                  </Form.Item>
+                  <Form.Item
+                    label={t('statement.settings.keep_duration')}
+                    extra={t('statement.settings.keep_duration_tooltip')}
+                    shouldUpdate={(prev, cur) =>
+                      prev.refresh_interval !== cur.refresh_interval ||
+                      prev.history_size !== cur.history_size
+                    }
+                  >
+                    {({ getFieldValue }) => {
+                      const refreshInterval =
+                        getFieldValue('refresh_interval') || 0
+                      const historySize = getFieldValue('history_size') || 0
+                      const totalMins = refreshInterval * historySize
+                      const day = Math.floor(totalMins / (24 * 60))
+                      const hour = Math.floor((totalMins - day * 24 * 60) / 60)
+                      const min = totalMins - day * 24 * 60 - hour * 60
+                      return `${day} day ${hour} hour ${min} min`
+                    }}
+                  </Form.Item>
+                  <Form.Item
+                    label={t('statement.settings.internal_query')}
+                    extra={t('statement.settings.internal_query_tooltip')}
+                    name="internal_query"
+                    valuePropName="checked"
+                  >
+                    <Switch />
+                  </Form.Item>
+                </>
               )
-            }}
+            }
           </Form.Item>
           <Form.Item>
             <Space>

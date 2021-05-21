eada29112793f394e607ce07226d3ea1e33a0fc1
Suhaha <jklopsdfw@gmail.com>
2021-05-21 09:33:17 +0800

    tweak(debugapi): test

--- pkg/apiserver/debugapi/endpoint_test.go
+++ pkg/apiserver/debugapi/endpoint_test.go
@@ -33,68 +33,105 @@ var _ = Suite(&testSchemaSuite{})
 
 type testSchemaSuite struct{}
 
-var testTiDBStatsDump EndpointAPIModel = EndpointAPIModel{
-	ID:        "tidb_stats_dump",
-	Component: model.NodeKindTiDB,
-	Path:      "/stats/dump/{db}/{table}",
-	Method:    http.MethodGet,
-	PathParams: []EndpointAPIParam{
-		{
-			Name:  "db",
-			Model: EndpointAPIParamModelText,
+func (t *testSchemaSuite) Test_NewRequest_with_path_param_success(c *C) {
+	testEndpoint := EndpointAPIModel{
+		ID:        "test_endpoint",
+		Component: model.NodeKindTiDB,
+		Path:      "/test/{param1}",
+		Method:    http.MethodGet,
+		PathParams: []EndpointAPIParam{
+			{
+				Name:  "param1",
+				Model: EndpointAPIParamModelText,
+			},
 		},
-		{
-			Name:  "table",
-			Model: EndpointAPIParamModelText,
-		},
-	},
-	QueryParams: []EndpointAPIParam{
-		{
-			Name:     "debug",
-			Required: true,
-			Model:    EndpointAPIParamModelInt,
-		},
-	},
-}
-
-func (t *testSchemaSuite) Test_new_request_success(c *C) {
-	db := "test"
-	table := "users"
-	debugFlag := "1"
-
-	vals := map[string]string{
-		"db":    db,
-		"table": table,
-		"debug": debugFlag,
 	}
-	req, err := testTiDBStatsDump.NewRequest("127.0.0.1", 10080, vals)
+	param1 := "param1"
 
+	req, err := testEndpoint.NewRequest("127.0.0.1", 10080, map[string]string{
+		"param1": param1,
+	})
 	if err == nil {
-		c.Assert(req.Path, Equals, fmt.Sprintf("/stats/dump/%s/%s", db, table))
-		c.Assert(req.Query, Equals, fmt.Sprintf("debug=%s", debugFlag))
+		c.Assert(req.Path, Equals, fmt.Sprintf("/test/%s", param1))
 	} else {
-		c.ExpectFailure(err.Error())
+		c.Error(err)
 	}
 }
 
-func (t *testSchemaSuite) Test_new_request_err_missing_required_path_params(c *C) {
-	vals := map[string]string{
-		"db":    "test",
-		"debug": "1",
+func (t *testSchemaSuite) Test_NewRequest_with_query_param_success(c *C) {
+	testEndpoint := EndpointAPIModel{
+		ID:        "test_endpoint",
+		Component: model.NodeKindTiDB,
+		Path:      "/test",
+		Method:    http.MethodGet,
+		QueryParams: []EndpointAPIParam{
+			{
+				Name:  "param1",
+				Model: EndpointAPIParamModelText,
+			},
+			{
+				Name:  "param2",
+				Model: EndpointAPIParamModelText,
+			},
+		},
 	}
-	_, err := testTiDBStatsDump.NewRequest("127.0.0.1", 10080, vals)
+	param1 := "param1"
+	param2 := "param2"
 
+	req, err := testEndpoint.NewRequest("127.0.0.1", 10080, map[string]string{
+		"param1": param1,
+	})
+	if err == nil {
+		c.Assert(req.Query, Equals, fmt.Sprintf("param1=%s", param1))
+	} else {
+		c.Error(err)
+	}
+
+	req2, err := testEndpoint.NewRequest("127.0.0.1", 10080, map[string]string{
+		"param1": param1,
+		"param2": param2,
+	})
+	if err == nil {
+		c.Assert(req2.Query, Equals, fmt.Sprintf("param1=%s&param2=%s", param1, param2))
+	} else {
+		c.Error(err)
+	}
+}
+
+func (t *testSchemaSuite) Test_NewRequest_missing_required_params_err(c *C) {
+	testEndpoint := EndpointAPIModel{
+		ID:        "test_endpoint",
+		Component: model.NodeKindTiDB,
+		Path:      "/test/{param1}",
+		Method:    http.MethodGet,
+		PathParams: []EndpointAPIParam{
+			{
+				Name:  "param1",
+				Model: EndpointAPIParamModelText,
+			},
+		},
+		QueryParams: []EndpointAPIParam{
+			{
+				Name:     "param2",
+				Model:    EndpointAPIParamModelText,
+				Required: true,
+			},
+		},
+	}
+	param1 := "param1"
+	param2 := "param2"
+
+	// missing path param
+	_, err := testEndpoint.NewRequest("127.0.0.1", 10080, map[string]string{
+		"param2": param2,
+	})
 	c.Log(err)
 	c.Assert(errorx.IsOfType(err, ErrMissingRequiredParam), Equals, true)
-}
 
-func (t *testSchemaSuite) Test_new_request_err_missing_required_query_params(c *C) {
-	vals := map[string]string{
-		"db":    "test",
-		"table": "users",
-	}
-	_, err := testTiDBStatsDump.NewRequest("127.0.0.1", 10080, vals)
-
+	// missing required query param
+	_, err = testEndpoint.NewRequest("127.0.0.1", 10080, map[string]string{
+		"param1": param1,
+	})
 	c.Log(err)
 	c.Assert(errorx.IsOfType(err, ErrMissingRequiredParam), Equals, true)
 }

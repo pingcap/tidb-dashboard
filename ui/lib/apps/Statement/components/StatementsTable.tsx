e05501eace2362d584917a2efa489c315848f9ff
Suhaha <jklopsdfw@gmail.com>
2021-09-28 17:01:08 +0800

    Release v2021.09.27.1 (#1014) [skip pd_pr]
    * fix(stmt): evict & unbound schema stmt query (#1006)
    
    * fix(client): goroutine leak (#1013)
    
    * Update release version
    
    * hide help link for non TiDB distro (#1015)
    
    Co-authored-by: Sparkle <1284531+baurine@users.noreply.github.com>

--- ui/lib/apps/Statement/components/StatementsTable.tsx
+++ ui/lib/apps/Statement/components/StatementsTable.tsx
@@ -1,6 +1,12 @@
 import { usePersistFn } from 'ahooks'
 import React, { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
+import {
+  DetailsRow,
+  IDetailsListProps,
+  IDetailsRowStyles,
+} from 'office-ui-fabric-react'
+import { getTheme } from 'office-ui-fabric-react/lib/Styling'
 
 import openLink from '@lib/utils/openLink'
 import { CardTable, ICardTableProps } from '@lib/components'
@@ -11,6 +17,8 @@ import { IStatementTableController } from '../utils/useStatementTableController'
 interface Props extends Partial<ICardTableProps> {
   controller: IStatementTableController
 }
+
+const theme = getTheme()
 
 export default function StatementsTable({ controller, ...restPrpos }: Props) {
   const {
@@ -30,6 +38,10 @@ export default function StatementsTable({ controller, ...restPrpos }: Props) {
   const navigate = useNavigate()
   const handleRowClick = usePersistFn(
     (rec, idx, ev: React.MouseEvent<HTMLElement>) => {
+      // the evicted record's digest is empty string
+      if (!rec.digest) {
+        return
+      }
       saveClickedItemIndex(idx)
       const qs = DetailPage.buildQuery({
         digest: rec.digest,
@@ -57,6 +69,24 @@ export default function StatementsTable({ controller, ...restPrpos }: Props) {
       onRowClicked={handleRowClick}
       getKey={getKey}
       clickedRowIndex={getClickedItemIndex()}
+      onRenderRow={renderRow}
     />
   )
+}
+
+const renderRow: IDetailsListProps['onRenderRow'] = (props) => {
+  if (!props) {
+    return null
+  }
+
+  const customStyles: Partial<IDetailsRowStyles> = {}
+  // the evicted record's digest is empty string
+  if (!props.item.digest) {
+    customStyles.root = {
+      backgroundColor: theme.palette.neutralLighter,
+      cursor: 'not-allowed',
+    }
+  }
+
+  return <DetailsRow {...props} styles={customStyles} />
 }

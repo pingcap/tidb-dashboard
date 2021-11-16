c5e60e572493a05856e7bd2fe2dfa4fe69595521
baurine <2008.hbl@gmail.com>
2021-11-16 13:08:11 +0800

    compile module less

--- ui-esbuild/src/App.tsx
+++ ui-esbuild/src/App.tsx
@@ -3,12 +3,14 @@ import './style.css'
 
 import HelloCSS from './lib/components/HelloCSS'
 import HelloLess from './lib/components/HelloLess'
+import HelloMLess from './lib/components/HelloModuleLess'
 
 export default function App() {
   return (
     <div>
       <HelloCSS />
       <HelloLess />
+      <HelloMLess />
     </div>
   )
 }
---
+++ ui-esbuild/src/lib/components/HelloModuleLess/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.less'
+
+export default function HelloMLess() {
+  return (
+    <div className={styles['hello-mless-container']}>
+      <h1>Hello Module Less</h1>
+    </div>
+  )
+}

2f4416108bdece3023465ea2622ab7f5d6faeea6
baurine <2008.hbl@gmail.com>
2021-11-17 18:31:54 +0800

    improve dev build speed

--- ui-esbuild/builder.js
+++ ui-esbuild/builder.js
@@ -25,11 +25,11 @@ const isDev = argv('dev') === true
 const serverParams = {
   port: 8181, // Set the server port. Defaults to 8080.
   root: 'dist', // Set root directory that's being served. Defaults to cwd.
-  open: true // When false, it won't load your browser by default.
+  open: false // When false, it won't load your browser by default.
   // host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   // ignore: 'scss,my/templates', // comma-separated string for paths to ignore
   // file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-  // wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  // wait: 1000 // Waits for all changes, before reloading. Defaults to 0 sec.
   // mount: [['/components', './node_modules']], // Mount a directory to a route.
   // logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
   // middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
@@ -90,10 +90,8 @@ const buildParams = {
 
 async function main() {
   // TODO - refine
-
   fs.rmSync('./dist', { force: true, recursive: true })
   fs.mkdirSync('./dist')
-
   fs.copyFileSync('./public/index.html', './dist/index.html')
   fs.copyFileSync('./public/favicon.ico', './dist/favicon.ico')
   fs.copyFileSync('./public/manifest.json', './dist/manifest.json')
@@ -103,11 +101,11 @@ async function main() {
   if (isDev) {
     const builder = await build(buildParams)
 
-    watch('src/**/*').on('all', () => {
+    start(serverParams)
+
+    watch('src/**/*', { ignoreInitial: true }).on('all', () => {
       builder.rebuild()
     })
-
-    start(serverParams)
   } else {
     build(buildParams).finally(() => process.exit(0))
   }

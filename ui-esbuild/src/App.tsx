c61ed0da8cc0324c35d4417657e77cdfb1808f51
baurine <2008.hbl@gmail.com>
2021-11-17 15:27:29 +0800

    test alias paths

--- ui-esbuild/src/App.tsx
+++ ui-esbuild/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import './style.less'
 
-import HelloCSS from './lib/test-components/HelloCSS'
-import HelloLess from './lib/test-components/HelloLess'
-import HelloMLess from './lib/test-components/HelloModuleLess'
-import HelloSCSS from './lib/test-components/HelloSCSS'
-import HelloAntD from './lib/test-components/HelloAntD'
-import HelloFluentUI from './lib/test-components/HelloFluentUI'
-import HelloAntDIcons from './lib/test-components/HelloAntDIcons'
-import HelloSVG from './lib/test-components/HelloSVG'
-import HelloYAML from './lib/test-components/HelloYAML'
+import {
+  HelloCSS,
+  HelloLess,
+  HelloMLess,
+  HelloSCSS,
+  HelloAntD,
+  HelloFluentUI,
+  HelloAntDIcons,
+  HelloSVG,
+  HelloYAML
+} from '@lib/test-components'
+
+import './style.less'
 
 export default function App() {
   return (

3b28574dcff1e64d96f79762b5ab8a2f8e84d5d7
baurine <2008.hbl@gmail.com>
2021-12-02 19:03:41 +0800

    refine

--- ui/lib/utils/sqlFormatter/index.ts
+++ ui/lib/utils/sqlFormatter/index.ts
@@ -2,9 +2,12 @@
 
 // const mySqlFormatter = new TiDBSQLFormatter({ uppercase: true })
 
+// export default function formatSql(sql?: string): string {
+//   return mySqlFormatter.format(sql || '')
+// }
+
 import { format } from 'sql-formatter'
 
 export default function formatSql(sql?: string): string {
-  // return mySqlFormatter.format(sql || '')
   return format(sql || '', { uppercase: true, language: 'mysql' })
 }

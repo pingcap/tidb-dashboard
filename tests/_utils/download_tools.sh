4712414edc7b160650bd9d5d5a07d03d620b796e
Suhaha <jklopsdfw@gmail.com>
2021-12-06 17:22:07 +0800

    test: update bin path

--- tests/_utils/download_tools.sh
+++ tests/_utils/download_tools.sh
@@ -8,4 +8,19 @@ echo "+ Check tiup command"
 if ! command -v tiup >/dev/null 2>&1; then
   echo "  - Download tiup"
   curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
+
+  # Update tiup bin path
+  echo "  - Update bin path"
+  shell=$(echo $SHELL | awk 'BEGIN {FS="/";} { print $NF }')
+  if [ -f "${HOME}/.${shell}_profile" ]; then
+      PROFILE=${HOME}/.${shell}_profile
+  elif [ -f "${HOME}/.${shell}_login" ]; then
+      PROFILE=${HOME}/.${shell}_login
+  elif [ -f "${HOME}/.${shell}rc" ]; then
+      PROFILE=${HOME}/.${shell}rc
+  else
+      PROFILE=${HOME}/.profile
+  fi
+
+  source $PROFILE
 fi

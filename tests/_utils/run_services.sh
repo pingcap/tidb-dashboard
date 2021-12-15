a1ce514ea86f41abad8ad67b966375852d1ccac5
Suhaha <jklopsdfw@gmail.com>
2021-12-15 12:00:17 +0800

    tweak: tiup teardown precisely

--- tests/_utils/run_services.sh
+++ tests/_utils/run_services.sh
@@ -2,16 +2,16 @@
 
 set -euo
 
-INTEGRATION_LOG=/tmp/dashboard-integration-test.log
-
+integration_log_path=/tmp/dashboard-integration-test.log
 tiup_bin_dir=$HOME/.tiup/bin
+playground_tag="integration_test"
 
 start_tidb() {
   echo "+ Waiting for TiDB start..."
 
-  rm -rf $INTEGRATION_LOG
+  rm -rf $integration_log_path
   tidb_version=${1:-latest}
-  $tiup_bin_dir/tiup playground $tidb_version > $INTEGRATION_LOG &
+  $tiup_bin_dir/tiup playground --tag $playground_tag $tidb_version > $integration_log_path &
   ensure_tidb
 
   echo "  - TiDB Version: $tidb_version, start success!"
@@ -19,14 +19,13 @@ start_tidb() {
 
 stop_tidb() {
   echo "+ Waiting for TiDB teardown..."
-  # TODO: clean the latest started playground
-  $tiup_bin_dir/tiup clean --all >/dev/null 2>&1
+  $tiup_bin_dir/tiup clean $playground_tag >/dev/null 2>&1
   echo "  - Stopped!"
 }
 
 ensure_tidb() {
   i=1
-  while ! grep "CLUSTER START SUCCESSFULLY" $INTEGRATION_LOG; do
+  while ! grep "CLUSTER START SUCCESSFULLY" $integration_log_path; do
     i=$((i+1))
     if [ "$i" -gt 20 ]; then
       echo 'Failed to start TiDB'

e9caf735905e7782e0ccfbe9b7e175e1f5f11978
Sparkle <1284531+baurine@users.noreply.github.com>
2021-12-22 14:50:34 +0800

    distro: support dynamic config (#1094)

--- scripts/distro/write_strings.sh
+++ scripts/distro/write_strings.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-# This script writes distribution strings in json which is required for the frontend.
+# This script outputs distribution strings to json which is required for the frontend.
 
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$(cd "$DIR/../.."; pwd)
-BUILD_TAG=""
 
-if [[ -f "${PROJECT_DIR}/internal/resource/distrores/strings.go" ]]; then
-  echo "+ Existing distribution resource is detected, using it to write strings"
-  BUILD_TAG=distro
-fi
-
-echo "+ Write resource strings"
+echo "+ Write distro strings"
 cd "$PROJECT_DIR"
-# FIXME: distro/write_strings needs to access the /internal package, which is not allowed to be invoked in another module
-# Currently we workaround this by invoking in the TiDB Dashboard module.
-go run -tags="${BUILD_TAG}" scripts/distro/write_strings.go -o="${PROJECT_DIR}/ui/lib/distribution.json"
+go run scripts/distro/write_strings.go -o="${PROJECT_DIR}/ui/lib/distro_strings.json"
 
-echo "  - Success! Resource strings:"
-cat "${PROJECT_DIR}/ui/lib/distribution.json"
+echo "  - Success! Distro strings:"
+cat "${PROJECT_DIR}/ui/lib/distro_strings.json"
 
 echo
--- ui/diagnoseReportApp/index.tsx
+++ ui/diagnoseReportApp/index.tsx
@@ -5,13 +5,15 @@ import 'bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
 import * as i18n from '@lib/utils/i18n'
+import { distro } from '@lib/utils/i18n'
+
 import DiagnosisReport from './components/DiagnosisReport'
 import translations from './translations'
+
 import './index.css'
 
 function refineDiagnosisData() {
   const diagnosisData = window.__diagnosis_data__ || []
-  console.log(window.__diagnosis_data__)
 
   let preCategory = ''
   diagnosisData.forEach((d) => {
@@ -25,6 +27,7 @@ function refineDiagnosisData() {
 }
 
 i18n.addTranslations(translations)
+document.title = `${distro.tidb} Dashboard Diagnosis Report`
 
 ReactDOM.render(
   <DiagnosisReport diagnosisTables={refineDiagnosisData()} />,

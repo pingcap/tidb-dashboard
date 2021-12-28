d35e112e410888a08545b3c9997ad1f5ffb82e8b
baurine <2008.hbl@gmail.com>
2021-12-28 17:56:16 +0800

    add test

--- tests/integration/user/user_test.go
+++ tests/integration/user/user_test.go
@@ -12,9 +12,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joomcode/errorx"
+	"github.com/pingcap/tidb-dashboard/pkg/apiserver/info"
 	"github.com/pingcap/tidb-dashboard/pkg/apiserver/user"
 	"github.com/pingcap/tidb-dashboard/pkg/apiserver/user/sqlauth"
 	"github.com/pingcap/tidb-dashboard/pkg/config"
+	"github.com/pingcap/tidb-dashboard/pkg/dbstore"
 	"github.com/pingcap/tidb-dashboard/pkg/httpc"
 	"github.com/pingcap/tidb-dashboard/pkg/pd"
 	"github.com/pingcap/tidb-dashboard/pkg/tidb"
@@ -30,6 +32,7 @@ type testUserSuite struct {
 	suite.Suite
 	db          *testutil.TestDB
 	authService *user.AuthService
+	infoService *info.Service
 }
 
 func TestUserSuite(t *testing.T) {
@@ -37,6 +40,7 @@ func TestUserSuite(t *testing.T) {
 	tidbVersion := util.GetTiDBVersion(t, db)
 
 	authService := &user.AuthService{}
+	infoService := &info.Service{}
 
 	app := fx.New(
 		fx.Supply(featureflag.NewRegistry(tidbVersion)),
@@ -46,9 +50,12 @@ func TestUserSuite(t *testing.T) {
 			pd.NewEtcdClient,
 			tidb.NewTiDBClient,
 			user.NewAuthService,
+			dbstore.NewDBStore,
+			info.NewService,
 		),
 		sqlauth.Module,
 		fx.Populate(&authService),
+		fx.Populate(&infoService),
 	)
 	ctx := context.Background()
 	app.Start(ctx)
@@ -56,6 +63,7 @@ func TestUserSuite(t *testing.T) {
 	suite.Run(t, &testUserSuite{
 		db:          db,
 		authService: authService,
+		infoService: infoService,
 	})
 
 	app.Stop(ctx)
@@ -114,8 +122,8 @@ func (s *testUserSuite) TestLoginWithNotExistUser() {
 	// when this case fails, it only updates context status and err, doesn't update and send response
 	s.authService.LoginHandler(c)
 
-	s.Require().True(errorx.IsOfType(c.Errors.Last().Err, tidb.ErrTiDBAuthFailed))
 	s.Require().Contains(c.Errors.Last().Err.Error(), "authenticate failed")
+	s.Require().True(errorx.IsOfType(c.Errors.Last().Err, tidb.ErrTiDBAuthFailed))
 	s.Require().Equal(401, c.Writer.Status())
 	s.Require().Equal(200, w.Code)
 }
@@ -133,8 +141,8 @@ func (s *testUserSuite) TestLoginWithWrongPassword() {
 	// when this case fails, it only updates context status and err, doesn't update and send response
 	s.authService.LoginHandler(c)
 
-	s.Require().True(errorx.IsOfType(c.Errors.Last().Err, tidb.ErrTiDBAuthFailed))
 	s.Require().Contains(c.Errors.Last().Err.Error(), "authenticate failed")
+	s.Require().True(errorx.IsOfType(c.Errors.Last().Err, tidb.ErrTiDBAuthFailed))
 	s.Require().Equal(401, c.Writer.Status())
 	s.Require().Equal(200, w.Code)
 }
@@ -156,4 +164,43 @@ func (s *testUserSuite) TestLoginWithInsufficientPrivs() {
 	s.Require().True(errorx.IsOfType(c.Errors.Last().Err, user.ErrInsufficientPrivs))
 	s.Require().Equal(401, c.Writer.Status())
 	s.Require().Equal(200, w.Code)
+}
+
+func (s *testUserSuite) TestLoginWithSufficientPrivs() {
+	if s.supportNonRootLogin() {
+		param := make(map[string]interface{})
+		param["type"] = 0
+		param["username"] = "dashboardAdmin"
+		param["password"] = "12345678"
+		jsonByte, _ := json.Marshal(param)
+
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		c.Request, _ = http.NewRequest(http.MethodPost, "/user/login", bytes.NewReader(jsonByte))
+		s.authService.LoginHandler(c)
+
+		s.Require().Len(c.Errors, 0)
+		s.Require().Equal(200, c.Writer.Status())
+		s.Require().Equal(200, w.Code)
+
+		res := struct {
+			Token string
+		}{}
+		err := json.Unmarshal(w.Body.Bytes(), &res)
+		s.Require().Nil(err)
+
+		// request /whoami by the token
+		w2 := httptest.NewRecorder()
+		c2, _ := gin.CreateTestContext(w2)
+		c2.Request, _ = http.NewRequest(http.MethodGet, "/info/whomai", nil)
+		c2.Request.Header.Add("Authorization", "Bearer "+res.Token)
+		s.authService.MWAuthRequired()(c2)
+		s.infoService.WhoamiHandler(c2)
+
+		s.Require().Equal(200, w2.Code)
+		res2 := info.WhoAmIResponse{}
+		err2 := json.Unmarshal(w2.Body.Bytes(), &res2)
+		s.Require().Nil(err2)
+		s.Require().Equal(res2.DisplayName, "dashboardAdmin")
+	}
 }

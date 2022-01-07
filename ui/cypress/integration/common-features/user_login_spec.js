dc4d899957cd470bc79315b251275644315af109
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2022-01-07 10:07:15 +0800

    test: add e2e tests (#1109)

---
+++ ui/cypress/integration/common-features/user_login_spec.js
@@ -0,0 +1,55 @@
+// Copyright 2021 PingCAP, Inc. Licensed under Apache-2.0.
+
+// Test User login
+describe('Root User Login', () => {
+  beforeEach(function () {
+    cy.fixture('uri.json').then(function (uri) {
+      this.uri = uri
+      cy.visit(this.uri.root)
+    })
+  })
+
+  it('authenticated redirect', function () {
+    // Redirect to login
+    cy.visit(this.uri.root)
+    cy.url().should('eq', `${Cypress.config().baseUrl}${this.uri.root}`)
+  })
+
+  it('root login with no pwd', function () {
+    cy.get('[data-e2e=signin_username_input]').should('have.value', 'root')
+    cy.get('[data-e2e=signin_submit]').click()
+    cy.url().should('include', `${this.uri.overview}`)
+  })
+
+  it('remember last succeeded login username', () => {
+    cy.get('[data-e2e=signin_username_input]').should('have.value', 'root')
+  })
+
+  it('root login with incorrect pwd', () => {
+    cy.intercept('POST', `${Cypress.env('apiUrl')}/user/login`).as('login')
+
+    // {enter} causes the form to submit
+    cy.get('[data-e2e="signin_password_input"]').type(
+      'incorrect_password{enter}'
+    )
+
+    cy.wait('@login').should(({ response }) => {
+      expect(response.body).to.have.property('code', 'tidb.tidb_auth_failed')
+    })
+  })
+
+  it('root login with correct pwd', function () {
+    // set password for root
+    let query = "SET PASSWORD FOR 'root'@'%' = 'root_pwd'"
+    let password = ''
+    cy.task('queryDB', { query, password })
+
+    cy.get('[data-e2e="signin_password_input"]').type('root_pwd{enter}')
+    cy.url().should('include', `${this.uri.overview}`)
+
+    // set empty password for root
+    query = "SET PASSWORD FOR 'root'@'%' = ''"
+    password = 'root_pwd'
+    cy.task('queryDB', { query, password })
+  })
+})

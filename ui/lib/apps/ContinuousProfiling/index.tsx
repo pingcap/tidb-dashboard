471fd0fe9b4163c8fbcd6e8bc2f0de1b87618f00
Suhaha <jklopsdfw@gmail.com>
2022-01-10 17:00:31 +0800

    tweak: conprof ngm not started guard

--- ui/lib/apps/ContinuousProfiling/index.tsx
+++ ui/lib/apps/ContinuousProfiling/index.tsx
@@ -2,24 +2,37 @@ import React from 'react'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Root, ParamsPageWrapper } from '@lib/components'
+
+import { NgmNotStartedGuard } from '../Ngm/components/Error/NgmNotStarted'
 import { Detail, List } from './pages'
 
-const App = () => (
-  <Root>
-    <Router>
-      <Routes>
-        <Route path="/continuous_profiling" element={<List />} />
-        <Route
-          path="/continuous_profiling/detail"
-          element={
-            <ParamsPageWrapper>
-              <Detail />
-            </ParamsPageWrapper>
-          }
-        />
-      </Routes>
-    </Router>
-  </Root>
-)
+const App = () => {
+  return (
+    <Root>
+      <Router>
+        <Routes>
+          <Route
+            path="/continuous_profiling"
+            element={
+              <NgmNotStartedGuard>
+                <List />
+              </NgmNotStartedGuard>
+            }
+          />
+          <Route
+            path="/continuous_profiling/detail"
+            element={
+              <NgmNotStartedGuard>
+                <ParamsPageWrapper>
+                  <Detail />
+                </ParamsPageWrapper>
+              </NgmNotStartedGuard>
+            }
+          />
+        </Routes>
+      </Router>
+    </Root>
+  )
+}
 
 export default App

507ba7720b44fa8d54146471e3a7196055a06891
Sparkle <1284531+baurine@users.noreply.github.com>
2022-01-20 20:09:41 +0800

    test: Add backend integration tests (#1111)

--- ui/lib/apps/Ngm/components/Error/NgmNotStarted.tsx
+++ ui/lib/apps/Ngm/components/Error/NgmNotStarted.tsx
@@ -1,4 +1,4 @@
-// Copyright 2021 PingCAP, Inc. Licensed under Apache-2.0.
+// Copyright 2022 PingCAP, Inc. Licensed under Apache-2.0.
 import React, { ReactNode } from 'react'
 import { Result } from 'antd'
 import { useTranslation } from 'react-i18next'

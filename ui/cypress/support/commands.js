4280fbaecb177f3fbc73b717175bbb2a2f15cbbf
Suhaha <jklopsdfw@gmail.com>
2022-02-09 09:55:14 +0800

    chore: adjust snapshot

--- ui/cypress/support/commands.js
+++ ui/cypress/support/commands.js
@@ -55,6 +55,8 @@ Cypress.Commands.overwrite(
   'matchImageSnapshot',
   (originalFn, snapshotName, options) => {
     if (Cypress.env('ALLOW_SCREENSHOT')) {
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000)
       originalFn(snapshotName, options)
     } else {
       cy.log(`Screenshot comparison is disabled`)

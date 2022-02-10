d1cbab3631f976925020071dcff075d76a26d6d3
Breezewish <hi@breeswish.org>
2022-02-10 19:07:37 +0800

    Fix lints

--- util/rest/context_helpers.go
+++ util/rest/context_helpers.go
@@ -11,22 +11,22 @@ import (
 	"github.com/pingcap/tidb-dashboard/util/jsonserde/ginadapter"
 )
 
-// TODO: Add tests
+// TODO: Add tests.
 func Error(c *gin.Context, err error) {
 	_ = c.Error(errorx.EnsureStackTrace(err))
 }
 
-// TODO: Add tests
+// TODO: Add tests.
 func JSON(c *gin.Context, code int, obj interface{}) {
 	c.Render(code, ginadapter.Renderer{Data: obj})
 }
 
-// TODO: Add tests
+// TODO: Add tests.
 func OK(c *gin.Context, obj interface{}) {
 	JSON(c, http.StatusOK, obj)
 }
 
-// TODO: Add tests
+// TODO: Add tests.
 func MustBind(c *gin.Context, obj interface{}) bool {
 	if err := c.ShouldBindWith(obj, ginadapter.Binding); err != nil {
 		Error(c, ErrBadRequest.WrapWithNoMessage(err))

ebdbc6b96f56220fc14f4f11343b3cbc878ae9c0
YiniXu9506 <xuyini@pingcap.com>
2022-02-21 13:54:49 +0800

    update slow query detail page e2e

--- ui/cypress/integration/slow_query/02-detail.spec.js
+++ ui/cypress/integration/slow_query/02-detail.spec.js
@@ -23,10 +23,15 @@ describe('Slow query detail page E2E test', () => {
     })
 
     it('Copy formatted sql to clipboard', () => {
+      // Prompt alert when testing copy to clipboard on chromium-based browser
+      // https://github.com/cypress-io/cypress/issues/2739
       cy.window().then((win) => {
         cy.stub(win, 'prompt').returns(win.prompt).as('copyToClipboardPrompt')
       })
 
+      // cypress cannot simulate  copy to the clipboard event,
+      // we need to use realClick to fire copy event.
+      // Related desc: https://github.com/dmtrKovalenko/cypress-real-events#why
       cy.get('[data-e2e=copy_formatted_sql_to_clipboard]')
         .realClick()
         .then(() => {
@@ -37,6 +42,25 @@ describe('Slow query detail page E2E test', () => {
         .realClick()
         .then(() => {
           cy.task('getClipboard').should('eq', 'SELECT sleep(1.2);')
+        })
+    })
+  })
+
+  describe('Plan descriptions', () => {
+    it('Check sql and default format', () => {
+      // sql is collapsed by default
+      cy.get('[data-e2e=expandText]').eq(1).should('have.text', 'Expand')
+    })
+  })
+
+  describe('Detail tabs', () => {
+    it('Check tabs list', () => {
+      const tabList = ['Basic', 'Time', 'Coprocessor', 'Transaction']
+      cy.get('[data-e2e=tabs]')
+        .find('.ant-tabs-tab')
+        .should('have.length', 4)
+        .each(($tab, index) => {
+          cy.wrap($tab).should('have.text', tabList[index])
         })
     })
   })

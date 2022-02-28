4e2da0d624a983ff92162f68abefedd482eb5940
Suhaha <jklopsdfw@gmail.com>
2022-02-28 15:42:10 +0800

    chore: add cpu time axis title

--- ui/lib/apps/TopSQL/pages/List/ListChart.tsx
+++ ui/lib/apps/TopSQL/pages/List/ListChart.tsx
@@ -79,6 +79,7 @@ export const ListChart = forwardRef<Chart, ListChartProps>(
         />
         <Axis
           id="left"
+          title="CPU Time"
           position={Position.Left}
           tickFormat={(v) => getValueFormat('ms')(v, 2)}
         />

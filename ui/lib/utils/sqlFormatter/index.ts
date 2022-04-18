c707c4a02b9635bfed7124604b672e6e4366e901
baurine <2008.hbl@gmail.com>
2022-04-18 22:58:32 +0800

    refine

--- ui/lib/utils/sqlFormatter/index.ts
+++ ui/lib/utils/sqlFormatter/index.ts
@@ -3,7 +3,7 @@ import { format } from '@baurine/sql-formatter-plus'
 export default function formatSql(sql?: string): string {
   let formatedSQL = sql || ''
   try {
-    formatedSQL = format(sql || '', { uppercase: true })
+    formatedSQL = format(sql || '', { uppercase: true, language: 'tidb' })
   } catch (err) {
     console.log(err)
     console.log(sql)

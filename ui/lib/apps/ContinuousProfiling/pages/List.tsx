a9ac833b25028fb3c1e69462fb30ece2f5c7def2
baurine <2008.hbl@gmail.com>
2022-04-20 11:15:12 +0800

    enable query when conprof is disabled

--- ui/lib/apps/ContinuousProfiling/pages/List.tsx
+++ ui/lib/apps/ContinuousProfiling/pages/List.tsx
@@ -1,13 +1,4 @@
-import {
-  Badge,
-  Tooltip,
-  Space,
-  Drawer,
-  Result,
-  Button,
-  Alert,
-  Form,
-} from 'antd'
+import { Badge, Tooltip, Space, Drawer, Button, Alert, Form } from 'antd'
 import { ScrollablePane } from 'office-ui-fabric-react/lib/ScrollablePane'
 import React, { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -207,7 +198,6 @@ export default function Page() {
               >
                 <DatePicker
                   showTime
-                  disabled={conprofIsDisabled}
                   onOpenChange={(open) =>
                     open && telemetry.openTimeRangePicker()
                   }
@@ -218,12 +208,7 @@ export default function Page() {
                 <span>-2h</span>
               </Form.Item>
               <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  loading={listLoading}
-                  disabled={conprofIsDisabled}
-                >
+                <Button type="primary" htmlType="submit" loading={listLoading}>
                   {t('conprof.list.toolbar.query')}
                 </Button>
               </Form.Item>
@@ -255,7 +240,7 @@ export default function Page() {
         </Toolbar>
       </Card>
 
-      {conprofIsDisabled && historyTable && historyTable.length > 0 && (
+      {conprofIsDisabled && (
         <div className={styles.alert_container}>
           <Alert
             message={t('conprof.settings.disabled_with_history')}
@@ -265,36 +250,18 @@ export default function Page() {
         </div>
       )}
 
-      {conprofIsDisabled && historyTable?.length === 0 ? (
-        <Result
-          title={t('conprof.settings.disabled_result.title')}
-          subTitle={t('conprof.settings.disabled_result.sub_title')}
-          extra={
-            <Button
-              type="primary"
-              onClick={() => {
-                setShowSettings(true)
-                telemetry.clickSettings('firstTimeTips')
-              }}
-            >
-              {t('conprof.settings.open_settings')}
-            </Button>
-          }
-        />
-      ) : (
-        <div style={{ height: '100%', position: 'relative' }}>
-          <ScrollablePane>
-            <CardTable
-              cardNoMarginTop
-              loading={listLoading}
-              items={historyTable || []}
-              columns={historyTableColumns}
-              errors={[historyError]}
-              onRowClicked={handleRowClick}
-            />
-          </ScrollablePane>
-        </div>
-      )}
+      <div style={{ height: '100%', position: 'relative' }}>
+        <ScrollablePane>
+          <CardTable
+            cardNoMarginTop
+            loading={listLoading}
+            items={historyTable || []}
+            columns={historyTableColumns}
+            errors={[historyError]}
+            onRowClicked={handleRowClick}
+          />
+        </ScrollablePane>
+      </div>
 
       <Drawer
         title={t('conprof.settings.title')}

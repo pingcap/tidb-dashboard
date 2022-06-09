7ac251b94d120dd959c464a4d452377d28861460
baurine <2008.hbl@gmail.com>
2022-06-09 15:05:27 +0800

    init eslint/prettier/husky configs

---
+++ ui-new/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    'react/react-in-jsx-scope': 'error',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'react/jsx-no-target-blank': 'off'
+  }
+}
---
+++ ui-new/README.md
@@ -0,0 +1,13 @@
+# TiDB Dashboard UI
+
+## Run
+
+### Dev
+
+1. install pnpm: `npm install -g pnpm`
+1. `pnpm i`
+1. `pnpm dev`
+
+### Build
+
+1. `pnpm build`

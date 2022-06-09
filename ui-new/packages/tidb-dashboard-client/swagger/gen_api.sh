d36c19d40f51bb8befca2b6590f09312bc9414ee
baurine <2008.hbl@gmail.com>
2022-06-09 16:52:18 +0800

    add tidb-dasbhoard-client package

---
+++ ui-new/packages/tidb-dashboard-client/swagger/gen_api.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR="$(dirname "$DIR")"
+
+cd $PROJECT_DIR/swagger
+
+API_DOC_DIR=$PROJECT_DIR/swagger/doc.json
+OPENAPI_CONFIG_DIR=$PROJECT_DIR/swagger/.openapi_config.yaml
+OUTPUT_DIR=$PROJECT_DIR/src/client/api
+
+pnpm openapi-generator-cli generate -i $API_DOC_DIR -g typescript-axios -c $OPENAPI_CONFIG_DIR -o $OUTPUT_DIR
+
+rm -rf $OUTPUT_DIR/.openapi-generator
+rm $OUTPUT_DIR/.gitignore
+rm $OUTPUT_DIR/.npmignore
+rm $OUTPUT_DIR/.openapi-generator-ignore
+rm $OUTPUT_DIR/git_push.sh

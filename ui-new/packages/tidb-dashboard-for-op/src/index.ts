d54ec49695d510eb8d93761ce2e85282835d42e8
baurine <2008.hbl@gmail.com>
2022-06-14 18:04:48 +0800

    copy dashboardApp to tidb-dashboard-for-op

---
+++ ui-new/packages/tidb-dashboard-for-op/src/index.ts
@@ -0,0 +1,5 @@
+// import './style.less'
+
+// import './sentry'
+
+import './dashboardApp'
---
+++ ui-new/packages/tidb-dashboard-for-op/src/react-app-env.d.ts
@@ -0,0 +1,14 @@
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.less' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.yaml' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}

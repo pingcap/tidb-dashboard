15ac40ca5b2f14cdb87297e66a9083798cb13158
baurine <2008.hbl@gmail.com>
2022-06-24 15:59:35 +0800

    slight refine

--- ui-new/packages/tidb-dashboard-for-op/src/uilts/publicPathPrefix.ts
+++ ui-new/packages/tidb-dashboard-for-op/src/uilts/publicPathPrefix.ts
@@ -6,7 +6,7 @@ let prefix =
     ?.getAttribute('content') || DEF_PUBLIC_PATH_PREFIX
 
 if (prefix === '__PUBLIC_PATH_PREFIX__') {
-  prefix = '/dashboard'
+  prefix = DEF_PUBLIC_PATH_PREFIX
 }
 
 export default prefix

e54d3854a26a4fbd79e2e2dcaab12908b9d06159
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 19:19:55 +0800

    Delete pkg directory

--- pkg/apiserver/profiling/module.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2022 PingCAP, Inc. Licensed under Apache-2.0.
-
-package profiling
-
-import "go.uber.org/fx"
-
-var Module = fx.Options(newFetchers, newService)
--- pkg/tikv/tikv.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 PingCAP, Inc. Licensed under Apache-2.0.
-
-package tikv
-
-import (
-	"github.com/joomcode/errorx"
-)
-
-var ErrNS = errorx.NewNamespace("error.tikv")

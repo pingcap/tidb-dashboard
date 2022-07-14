e6452cfd118e6a460882b52b6bd4921eb93487ff
YiniXu9506 <xuyini@pingcap.com>
2022-07-14 15:05:39 +0800

    refine metrics plan

--- ui/lib/apps/Overview/pages/Detail/index.tsx
+++ ui/lib/apps/Overview/pages/Detail/index.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import Metrics from '../../components/Metrics'
 
 export default function Detail() {
-  return <Metrics allTypes={true} />
+  return <Metrics showAllPanels={true} />
 }

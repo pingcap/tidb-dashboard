2187665a49e89a87c5b39b426f455068ad793702
YiniXu9506 <xuyini@pingcap.com>
2022-07-19 12:16:21 +0800

    update translations

--- ui/lib/components/TreeDiagram/NodeDetail/DefaultNodeDetail.tsx
+++ ui/lib/components/TreeDiagram/NodeDetail/DefaultNodeDetail.tsx
@@ -5,48 +5,11 @@ import { toFixed, getValueFormat } from '@baurine/grafana-value-formats'
 import { Tabs, Tooltip } from 'antd'
 import { InfoCircleTwoTone } from '@ant-design/icons'
 
-import { addTranslationResource } from '@lib/utils/i18n'
+import { addTranslations } from '@lib/utils/i18n'
 import { useTranslation } from 'react-i18next'
+import translations from '../translations'
 
-const translations = {
-  en: {
-    tooltip: {
-      duration:
-        'The time taken by the parent operator includes the time taken by all children.',
-    },
-    diagnosis: {
-      high_est_error: 'The estimation error is high. Consider checking the health state of the statistics.',
-      disk_spill: 'Disk spill is triggered for this operator because the memory quota is exceeded. The execution might be slow. Consider increasing the memory quota if there\'s enough memory.',
-      pseudo_est: 'This operator used pseudo statistics and the estimation might be inaccurate. It might be caused by unavailable or outdated statistics. Consider collecting statistics or setting variable tidb_enable_pseudo_for_outdated_stats to OFF.',
-      good_filter_on_table_fullscan: 'This Selection filters a high proportion of data. Using an index on this column might achieve better performance. Consider adding an index on this column if there is not one.',
-      bad_index_for_index_lookup: 'This IndexLookup read a lot of data from the index side. It might be slow and cause heavy pressure on TiKV. Consider using the optimizer hints to guide the optimizer to choose a better index or not to use index.',
-      index_join_build_side_too_large: 'This index join has a large build side. It might be slow and cause heavy pressure on TiKV. Consider using the optimizer hints to guide the optimizer to choose hash join.',
-      tikv_huge_table_scan: 'The TiKV read a lot of data. Consider using TiFlash to get better performance if it\'s necessary to read so much data.',
-    },
-  },
-  zh: {
-    tooltip: {
-      duration: '父算子的耗时包含所有子算子的耗时。',
-    },
-    diagnosis: {
-      high_est_error: 'The estimation error is high. Consider checking the health state of the statistics.',
-      disk_spill: 'Disk spill is triggered for this operator because the memory quota is exceeded. The execution might be slow. Consider increasing the memory quota if there\'s enough memory.',
-      pseudo_est: 'This operator used pseudo statistics and the estimation might be inaccurate. It might be caused by unavailable or outdated statistics. Consider collecting statistics or setting variable tidb_enable_pseudo_for_outdated_stats to OFF.',
-      good_filter_on_table_fullscan: 'This Selection filters a high proportion of data. Using an index on this column might achieve better performance. Consider adding an index on this column if there is not one.',
-      bad_index_for_index_lookup: 'This IndexLookup read a lot of data from the index side. It might be slow and cause heavy pressure on TiKV. Consider using the optimizer hints to guide the optimizer to choose a better index or not to use index.',
-      index_join_build_side_too_large: 'This index join has a large build side. It might be slow and cause heavy pressure on TiKV. Consider using the optimizer hints to guide the optimizer to choose hash join.',
-      tikv_huge_table_scan: 'The TiKV read a lot of data. Consider using TiFlash to get better performance if it\'s necessary to read so much data.',
-    },
-  },
-}
-
-for (const key in translations) {
-  addTranslationResource(key, {
-    component: {
-      binaryPlan: translations[key],
-    },
-  })
-}
+addTranslations(translations)
 
 export const DefaultNodeDetail = (nodeDetailProps) => {
   const nodeDatum = nodeDetailProps.data
@@ -57,7 +20,7 @@ export const DefaultNodeDetail = (nodeDetailProps) => {
       <Tabs.TabPane tab="General" key="1" style={{ padding: '1rem' }}>
         <p>
           Duration{' '}
-          <Tooltip title={t(`component.binaryPlan.tooltip.duration`)}>
+          <Tooltip title={t(`binary_plan.tooltip.duration`)}>
             <InfoCircleTwoTone style={{ paddingRight: 5 }} />
           </Tooltip>
           : <span>{nodeDatum.duration} </span>
@@ -180,7 +143,7 @@ export const DefaultNodeDetail = (nodeDetailProps) => {
         <Tabs.TabPane tab="Diagnose" key="4">
           <ol type="1">
             {nodeDatum.diagnosis.map((d, idx) => (
-              <li key={idx} style={{ padding: '1rem 0'}}>{t(`component.binaryPlan.diagnosis.${d}`)}</li>
+              <li key={idx} style={{ padding: '1rem 0'}}>{t(`binary_plan.diagnosis.${d}`)}</li>
             ))}
           </ol>
         </Tabs.TabPane>

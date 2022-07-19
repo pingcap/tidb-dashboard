32880f16f89be5bedde0f8f85a2be3d16d33d3c4
baurine <2008.hbl@gmail.com>
2022-07-19 17:40:35 +0800

    just keep slow query apis

--- ui/packages/clinic-client/swagger/gen_api.sh
+++ ui/packages/clinic-client/swagger/gen_api.sh
@@ -11,9 +11,9 @@ OUTPUT_DIR=$PROJECT_DIR/src/client/api
 
 cd $PROJECT_DIR/swagger
 
-touch spec.json && rm spec.json
+# touch spec.json && rm spec.json
 
-curl -o spec.json -fsSL http://clinic-staging-1072990385.us-west-2.elb.amazonaws.com:8085/swagger/doc.json
+# curl -o spec.json -fsSL http://clinic-staging-1072990385.us-west-2.elb.amazonaws.com:8085/swagger/doc.json
 
 pnpm openapi-generator-cli generate -i $API_SPEC_DIR -g typescript-axios -c $OPENAPI_CONFIG_DIR -o $OUTPUT_DIR
 

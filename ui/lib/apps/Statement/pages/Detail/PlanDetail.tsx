95a408f9fae803977a27b425f612eec002e575c1
YiniXu9506 <xuyini@pingcap.com>
2022-07-19 17:31:47 +0800

    chore: fix Execution plan tab

--- ui/lib/apps/Statement/pages/Detail/PlanDetail.tsx
+++ ui/lib/apps/Statement/pages/Detail/PlanDetail.tsx
@@ -164,96 +164,95 @@ function PlanDetail({ query }: IPlanDetailProps) {
             </Descriptions>
 
             {(binaryPlan || data.plan) && (
-              <Descriptions>
-                <Descriptions.Item
-                  label={
-                    <Space align="baseline" size="middle">
-                      <span style={{ paddingRight: '2rem' }}>
-                        {t('slow_query.detail.plan.title')}
-                      </span>
-                    </Space>
+              <>
+                <Space
+                  size="middle"
+                  style={{ color: '#8c8c8c' }}
+                >
+                  {t('statement.pages.detail.desc.plans.execution.title')}
+                </Space>
+
+                <Tabs
+                  defaultActiveKey={
+                    binaryPlan && !binaryPlan.main.discardedDueToTooLong
+                      ? 'binary_plan'
+                      : 'text_plan'
                   }
                 >
-                   <Tabs
-                        defaultActiveKey={
-                          binaryPlan && !binaryPlan.main.discardedDueToTooLong
-                            ? 'binary_plan'
-                            : 'text_plan'
+                  {binaryPlan && !binaryPlan.main.discardedDueToTooLong && (
+                    <Tabs.TabPane
+                      tab={t(
+                        'statement.pages.detail.desc.plans.execution.visual'
+                      )}
+                      key="binary_plan"
+                    >
+                      <Modal
+                        title={t(
+                          'statement.pages.detail.desc.plans.execution.modal_title'
+                        )}
+                        centered
+                        visible={isVpVisible}
+                        width={window.innerWidth}
+                        onCancel={toggleVisualPlan}
+                        footer={null}
+                        destroyOnClose={true}
+                        bodyStyle={{
+                          background: '#f5f5f5',
+                          height: window.innerHeight - 100,
+                        }}
+                      >
+                        <TreeDiagramView
+                          data={
+                            binaryPlan.ctes
+                              ? [binaryPlan.main].concat(binaryPlan.ctes)
+                              : [binaryPlan.main]
+                          }
+                          showMinimap={true}
+                        />
+                      </Modal>
+                      <Descriptions>
+                        <Descriptions.Item span={2}>
+                          <div onClick={toggleVisualPlan}>
+                            <TreeDiagramView
+                              data={
+                                binaryPlan.ctes
+                                  ? [binaryPlan.main].concat(binaryPlan.ctes)
+                                  : [binaryPlan.main]
+                              }
+                              isThumbnail={true}
+                            />
+                          </div>
+                        </Descriptions.Item>
+                      </Descriptions>
+                    </Tabs.TabPane>
+                  )}
+
+                  <Tabs.TabPane
+                    tab={t('statement.pages.detail.desc.plans.execution.text')}
+                    key="text_plan"
+                  >
+                    <Descriptions>
+                      <Descriptions.Item
+                        span={2}
+                        multiline={detailExpand.plan}
+                        label={
+                          <Space size="middle">
+                            <Expand.Link
+                              expanded={detailExpand.plan}
+                              onClick={togglePlan}
+                            />
+                            <CopyLink data={data.plan ?? ''} />
+                          </Space>
                         }
                       >
-                        {binaryPlan && !binaryPlan.main.discardedDueToTooLong && (
-                          <Tabs.TabPane
-                            tab={t('slow_query.detail.plan.visual')}
-                            key="binary_plan"
-                          >
-                            <Modal
-                              title={t('slow_query.detail.plan.modal_title')}
-                              centered
-                              visible={isVpVisible}
-                              width={window.innerWidth}
-                              onCancel={toggleVisualPlan}
-                              footer={null}
-                              destroyOnClose={true}
-                              bodyStyle={{
-                                background: '#f5f5f5',
-                                height: window.innerHeight - 100,
-                              }}
-                            >
-                              <TreeDiagramView
-                                data={
-                                  binaryPlan.ctes
-                                    ? [binaryPlan.main].concat(binaryPlan.ctes)
-                                    : [binaryPlan.main]
-                                }
-                                showMinimap={true}
-                              />
-                            </Modal>
-                            <Descriptions>
-                              <Descriptions.Item span={2}>
-                                <div onClick={toggleVisualPlan}>
-                                  <TreeDiagramView
-                                    data={
-                                      binaryPlan.ctes
-                                        ? [binaryPlan.main].concat(
-                                            binaryPlan.ctes
-                                          )
-                                        : [binaryPlan.main]
-                                    }
-                                    isThumbnail={true}
-                                  />
-                                </div>
-                              </Descriptions.Item>
-                            </Descriptions>
-                          </Tabs.TabPane>
-                        )}
-
-                        <Tabs.TabPane
-                          tab={t('slow_query.detail.plan.text')}
-                          key="text_plan"
-                        >
-                          <Descriptions>
-                            <Descriptions.Item
-                              span={2}
-                              multiline={detailExpand.plan}
-                              label={
-                                <Space size="middle">
-                                  <Expand.Link
-                                    expanded={detailExpand.plan}
-                                    onClick={togglePlan}
-                                  />
-                                  <CopyLink data={data.plan ?? ''} />
-                                </Space>
-                              }
-                            >
-                              <Expand expanded={detailExpand.plan}>
-                                <Pre noWrap>{data.plan}</Pre>
-                              </Expand>
-                            </Descriptions.Item>
-                          </Descriptions>
-                        </Tabs.TabPane>
-                      </Tabs>
-                </Descriptions.Item>
-              </Descriptions>
+                        <Expand expanded={detailExpand.plan}>
+                          <Pre noWrap>{data.plan}</Pre>
+                        </Expand>
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Tabs.TabPane>
+                </Tabs>
+              </>
             )}
 
             <DetailTabs data={data} query={query} />

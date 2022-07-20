7bc39a87788781b23fff7930e7f11e8e89bc156d
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2022-07-20 16:20:26 +0800

    feat: extract metrics from overview app (#1337)
    * chore: extract metrics from overview app
    
    * update metrics app on dbaas
    
    * update side menu and metrics panel ui
    
    * resolve comments
    
    * chore: update metrics query
    
    * chore: add new metrics
    
    * chore: update promql rate_interval
    
    * remove useless file
    
    * remove un-used apis
    
    * align the chart and toolbar
    
    Co-authored-by: Suhaha <jklopsdfw@gmail.com>
    Co-authored-by: baurine <2008.hbl@gmail.com>

---
+++ ui/packages/tidb-dashboard-for-dbaas/src/apps/Metrics/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MetricsApp, MetricsProvider } from '@pingcap/tidb-dashboard-lib'
+import { ctx } from './context'
+
+export default function () {
+  return (
+    <MetricsProvider value={ctx()}>
+      <MetricsApp />
+    </MetricsProvider>
+  )
+}

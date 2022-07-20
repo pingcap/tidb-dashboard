1f623972a5406c1b227cbb6ebcd9cd42639ed0bb
Suhaha <jklopsdfw@gmail.com>
2022-07-20 22:22:48 +0800

    fix: Unnecessary render (#1349)
    * fix: Unnecessary render
    
    * chore: use debounce

--- ui/packages/tidb-dashboard-lib/src/apps/Metrics/components/Metrics.tsx
+++ ui/packages/tidb-dashboard-lib/src/apps/Metrics/components/Metrics.tsx
@@ -1,5 +1,5 @@
 import { Space, Typography, Row, Col, Collapse } from 'antd'
-import React, { useContext, useMemo, useState } from 'react'
+import React, { useCallback, useContext, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
   AutoRefreshButton,
@@ -10,57 +10,16 @@ import {
   TimeRangeSelector,
   Toolbar
 } from '@lib/components'
-import { Range } from '@elastic/charts/dist/utils/domain'
 import { Stack } from 'office-ui-fabric-react'
 import { useTimeRangeValue } from '@lib/components/TimeRangeSelector/hook'
 import { LoadingOutlined } from '@ant-design/icons'
-import { some } from 'lodash'
-import { ReqConfig } from '@lib/types'
-import { MetricsQueryResponse } from '@lib/client'
-import { AxiosPromise } from 'axios'
 import { MetricsContext } from '../context'
 
 import { PointerEvent } from '@elastic/charts'
 import { ChartContext } from '@lib/components/MetricChart/ChartContext'
-import { useEventEmitter } from 'ahooks'
+import { useEventEmitter, useMemoizedFn } from 'ahooks'
 import { metricsItems } from '../data/metricsItems'
-
-interface IChartProps {
-  range: Range
-  onRangeChange?: (newRange: Range) => void
-  onLoadingStateChange?: (isLoading: boolean) => void
-  getMetrics: (
-    endTimeSec?: number,
-    query?: string,
-    startTimeSec?: number,
-    stepSec?: number,
-    options?: ReqConfig
-  ) => AxiosPromise<MetricsQueryResponse>
-}
-
-const MetricsWrapper = ({ metricsItem, props }) => {
-  return (
-    <Card
-      noMargin
-      style={{
-        border: '1px solid #f1f0f0',
-        padding: '10px 2rem',
-        backgroundColor: '#fcfcfd'
-      }}
-    >
-      <Typography.Title level={5} style={{ textAlign: 'center' }}>
-        {metricsItem.title}
-      </Typography.Title>
-      <MetricChart
-        queries={metricsItem.queries}
-        type={metricsItem.type}
-        unit={metricsItem.unit}
-        nullValue={metricsItem.nullValue}
-        {...props}
-      />
-    </Card>
-  )
-}
+import { debounce } from 'lodash'
 
 export default function Metrics() {
   const ctx = useContext(MetricsContext)
@@ -68,22 +27,21 @@ export default function Metrics() {
 
   const [timeRange, setTimeRange] = useState<TimeRange>(DEFAULT_TIME_RANGE)
   const [chartRange, setChartRange] = useTimeRangeValue(timeRange, setTimeRange)
-  const [isLoading, setIsLoading] = useState<Record<string, boolean>>({})
+  const loadingCounter = useRef(0)
+  const [isSomeLoading, setIsSomeLoading] = useState(false)
 
-  const isSomeLoading = useMemo(() => {
-    return some(Object.values(isLoading))
-  }, [isLoading])
+  // eslint-disable-next-line
+  const setIsSomeLoadingDebounce = useCallback(
+    debounce(setIsSomeLoading, 100, { leading: true }),
+    []
+  )
 
-  function metricProps(id: string): IChartProps {
-    return {
-      range: chartRange,
-      onRangeChange: setChartRange,
-      onLoadingStateChange: (loading) =>
-        setIsLoading((v) => ({ ...v, [id]: loading })),
-      getMetrics: ctx!.ds.metricsQueryGet
-    }
-  }
-
+  const onLoadingStateChange = useMemoizedFn((loading: boolean) => {
+    loading
+      ? (loadingCounter.current += 1)
+      : loadingCounter.current > 0 && (loadingCounter.current -= 1)
+    setIsSomeLoadingDebounce(loadingCounter.current > 0)
+  })
   return (
     <>
       <Card>
@@ -119,10 +77,31 @@ export default function Metrics() {
                   <Row gutter={[16, 16]}>
                     {item.metrics.map((m) => (
                       <Col xl={12} sm={24} key={m.title}>
-                        <MetricsWrapper
-                          metricsItem={m}
-                          props={metricProps(`${m.title}`)}
-                        />
+                        <Card
+                          noMargin
+                          style={{
+                            border: '1px solid #f1f0f0',
+                            padding: '10px 2rem',
+                            backgroundColor: '#fcfcfd'
+                          }}
+                        >
+                          <Typography.Title
+                            level={5}
+                            style={{ textAlign: 'center' }}
+                          >
+                            {m.title}
+                          </Typography.Title>
+                          <MetricChart
+                            queries={m.queries}
+                            type={m.type}
+                            unit={m.unit}
+                            nullValue={m.nullValue}
+                            range={chartRange}
+                            onRangeChange={setChartRange}
+                            getMetrics={ctx!.ds.metricsQueryGet}
+                            onLoadingStateChange={onLoadingStateChange}
+                          />
+                        </Card>
                       </Col>
                     ))}
                   </Row>

a9841026c1507d60ac1f612b120f7c827ff67684
Zhenchi <zhongzc_arch@outlook.com>
2022-07-21 11:07:46 +0800

    Revert "bump grpc (#1323)" (#1356)
    This reverts commit 250a148ec78717b1aa75420f5f6472fa57414b13.

--- pkg/apiserver/conprof/service.go
+++ pkg/apiserver/conprof/service.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/clientv3"
 	"go.uber.org/fx"
 
 	"github.com/pingcap/tidb-dashboard/pkg/apiserver/user"

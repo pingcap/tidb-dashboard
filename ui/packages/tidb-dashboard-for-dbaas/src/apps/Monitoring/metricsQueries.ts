0ba5d6dd9ce241b675225486415bb4e72194b055
YiniXu9506 <xuyini@pingcap.com>
2022-07-29 15:21:37 +0800

    unify unit and update tikv io write query

--- ui/packages/tidb-dashboard-for-dbaas/src/apps/Monitoring/metricsQueries.ts
+++ ui/packages/tidb-dashboard-for-dbaas/src/apps/Monitoring/metricsQueries.ts
@@ -631,7 +631,7 @@ const monitoringItems: MetricsQueryType[] = [
         queries: [
           {
             query:
-              'sum(rate(tikv_engine_flow_bytes{db="kv", type="wal_file_bytes"}[$__rate_interval])) by (instance) + (sum(rate(tikv_engine_flow_bytes{db="raft", type="wal_file_bytes"}[$__rate_interval])) by (instance) + (sum(rate(raft_engine_write_size_sum[$__rate_interval])) by (instance)',
+              'sum(rate(tikv_engine_flow_bytes{db="kv", type="wal_file_bytes"}[2m])) by (instance) + (sum(rate(tikv_engine_flow_bytes{db="raft", type="wal_file_bytes"}[2m])) by (instance) or (0 * sum(rate(raft_engine_write_size_sum[2m])) by (instance))) + (sum(rate(raft_engine_write_size_sum[2m])) by (instance) or (0 * sum(rate(tikv_engine_flow_bytes{db="raft", type="wal_file_bytes"}[2m])) by (instance)))',
             name: '{instance}-write'
           },
           {
@@ -651,7 +651,7 @@ const monitoringItems: MetricsQueryType[] = [
             name: '{instance}'
           }
         ],
-        unit: 'decbytes',
+        unit: 'bytes',
         type: 'area_stack'
       },
       {

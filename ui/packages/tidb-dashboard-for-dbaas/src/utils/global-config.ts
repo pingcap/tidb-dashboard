02aad36dbf6a019209e49e8778258b106d99f551
baurine <2008.hbl@gmail.com>
2022-10-24 14:03:09 +0800

    feat: add telemetry for cluster info

--- ui/packages/tidb-dashboard-for-dbaas/src/utils/global-config.ts
+++ ui/packages/tidb-dashboard-for-dbaas/src/utils/global-config.ts
@@ -6,6 +6,14 @@ export interface IGlobalConfig {
   mixpanelUser: string
   timezone: number | null
   promBaseUrl: string
+
+  clusterInfo: {
+    orgId: string
+    tenantPlan: string // FREE_TRIAL / POC / ON_DEMAND
+    projectId: string
+    clusterId: string
+    deployType: string // Dedicated / Dev Tier
+  }
 }
 
 export const GlobalConfigContext = createContext<IGlobalConfig | null>(null)

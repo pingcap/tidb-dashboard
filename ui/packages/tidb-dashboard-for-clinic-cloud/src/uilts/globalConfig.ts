b8e8412e2e944a7f1eb1f5b6f74dafccc53e6aa4
baurine <2008.hbl@gmail.com>
2022-12-02 16:58:55 +0800

    control overview whether show more metrics buttton

--- ui/packages/tidb-dashboard-for-clinic-cloud/src/uilts/globalConfig.ts
+++ ui/packages/tidb-dashboard-for-clinic-cloud/src/uilts/globalConfig.ts
@@ -1,4 +1,8 @@
-import { ISlowQueryConfig, ITopSQLConfig } from '@pingcap/tidb-dashboard-lib'
+import {
+  IOverviewConfig,
+  ISlowQueryConfig,
+  ITopSQLConfig
+} from '@pingcap/tidb-dashboard-lib'
 
 export type AppOptions = {
   lang: string
@@ -33,6 +37,7 @@ export type ClusterInfo = {
 }
 
 export type AppsConfig = {
+  overview?: Partial<IOverviewConfig>
   slowQuery?: Partial<ISlowQueryConfig>
   topSQL?: Partial<ITopSQLConfig>
 }

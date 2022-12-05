6979a9fdf4f19e3716bf8d8e6c644894b2029f56
Sparkle <1284531+baurine@users.noreply.github.com>
2022-12-05 09:41:36 +0800

    feat: support disable or enable apps (#1448)
    * control overview whether show more metrics buttton
    
    * enable to disable apps
    
    * support enable or disable some apps
    
    * bump tidb-dashboard-for-clinic-cloud to 0.0.28
    
    (cherry picked from commit 574130072df0b9f509e200cebe245d8cfc0c3c59)

--- ui/packages/tidb-dashboard-for-clinic-cloud/src/apps/Overview/index.tsx
+++ ui/packages/tidb-dashboard-for-clinic-cloud/src/apps/Overview/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { OverviewApp, OverviewProvider } from '@pingcap/tidb-dashboard-lib'
+import { getGlobalConfig } from '~/uilts/globalConfig'
 import { ctx } from './context'
 
 export default function () {
   return (
-    <OverviewProvider value={ctx}>
+    <OverviewProvider value={ctx(getGlobalConfig().appsConfig?.overview || {})}>
       <OverviewApp />
     </OverviewProvider>
   )

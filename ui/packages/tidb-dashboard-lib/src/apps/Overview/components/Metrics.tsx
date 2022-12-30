ef1ec7363638e93949ecd013ac2331dff8f4f80b
YiniXu9506 <xuyini@pingcap.com>
2022-12-30 14:52:23 +0800

    chore: update time range selector no overview page

--- ui/packages/tidb-dashboard-lib/src/apps/Overview/components/Metrics.tsx
+++ ui/packages/tidb-dashboard-lib/src/apps/Overview/components/Metrics.tsx
@@ -69,20 +69,30 @@ export default function Metrics() {
       <Card>
         <Toolbar>
           <Space>
-            <TimeRangeSelector.WithZoomOut
-              value={timeRange}
-              onChange={(v) => {
-                setTimeRange(v)
-                telemetry.selectTimeRange(v)
-              }}
-              recent_seconds={ctx?.cfg.timeRangeSelector?.recent_seconds}
-              customAbsoluteRangePicker={
-                ctx?.cfg.timeRangeSelector?.customAbsoluteRangePicker
-              }
-              onZoomOutClick={(start, end) =>
-                telemetry.clickZoomOut([start, end])
-              }
-            />
+            {ctx?.cfg.timeRangeSelector?.customAbsoluteRangePicker ? (
+              <TimeRangeSelector
+                value={timeRange}
+                onChange={(v) => {
+                  setTimeRange(v)
+                  telemetry.selectTimeRange(v)
+                }}
+                recent_seconds={ctx?.cfg.timeRangeSelector?.recent_seconds}
+                customAbsoluteRangePicker={true}
+              />
+            ) : (
+              <TimeRangeSelector.WithZoomOut
+                value={timeRange}
+                onChange={(v) => {
+                  setTimeRange(v)
+                  telemetry.selectTimeRange(v)
+                }}
+                recent_seconds={ctx?.cfg.timeRangeSelector?.recent_seconds}
+                customAbsoluteRangePicker={false}
+                onZoomOutClick={(start, end) =>
+                  telemetry.clickZoomOut([start, end])
+                }
+              />
+            )}
             <AutoRefreshButton
               onChange={telemetry.selectAutoRefreshOption}
               onRefresh={handleManualRefreshClick}

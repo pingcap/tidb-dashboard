bfe321ff8369f5c872d7d0356d4d65809b53830d
Suhaha <jklopsdfw@gmail.com>
2023-02-08 23:50:00 +0800

    feat(slowquery): add warnings (#1472)
    * feat(slowquery): add warnings
    
    * fix: lint generic
    
    * chore: types
    
    * fix: test
    
    * fix: test
    
    * fix: tests

--- ui/packages/tidb-dashboard-lib/src/apps/SlowQuery/pages/Detail/DetailTabs.tsx
+++ ui/packages/tidb-dashboard-lib/src/apps/SlowQuery/pages/Detail/DetailTabs.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
+import ReactJson from 'react-json-view'
 
 import { SlowqueryModel } from '@lib/client'
 import { valueColumns, timeValueColumns } from '@lib/utils/tableColumns'
@@ -19,76 +20,98 @@ export default function DetailTabs({ data }: { data: SlowqueryModel }) {
   const { schemaColumns } = useSchemaColumns(
     ctx!.ds.slowQueryAvailableFieldsGet
   )
-  const columnsSet = new Set(schemaColumns)
 
-  const tabs = [
-    {
-      key: 'basic',
-      title: t('slow_query.detail.tabs.basic'),
-      content: () => {
-        const items = tabBasicItems(data)
-        const columns = valueColumns('slow_query.fields.')
-        return (
-          <CardTable
-            cardNoMargin
-            columns={columns}
-            items={items}
-            extendLastColumn
-            data-e2e="details_list"
-          />
-        )
+  const tabs = useMemo(() => {
+    const tbs = [
+      {
+        key: 'basic',
+        title: t('slow_query.detail.tabs.basic'),
+        content: () => {
+          const items = tabBasicItems(data)
+          const columns = valueColumns('slow_query.fields.')
+          return (
+            <CardTable
+              cardNoMargin
+              columns={columns}
+              items={items}
+              extendLastColumn
+              data-e2e="details_list"
+            />
+          )
+        }
+      },
+      {
+        key: 'time',
+        title: t('slow_query.detail.tabs.time'),
+        content: () => {
+          const items = tabTimeItems(data, t)
+          const columns = timeValueColumns('slow_query.fields.', items)
+          return (
+            <CardTable
+              cardNoMargin
+              columns={columns}
+              items={items}
+              extendLastColumn
+            />
+          )
+        }
+      },
+      {
+        key: 'copr',
+        title: t('slow_query.detail.tabs.copr'),
+        content: () => {
+          const columnsSet = new Set(schemaColumns)
+          const items = tabCoprItems(data).filter((item) =>
+            columnsSet.has(item.key)
+          )
+          const columns = valueColumns('slow_query.fields.')
+          return (
+            <CardTable
+              cardNoMargin
+              columns={columns}
+              items={items}
+              extendLastColumn
+            />
+          )
+        }
+      },
+      {
+        key: 'txn',
+        title: t('slow_query.detail.tabs.txn'),
+        content: () => {
+          const items = tabTxnItems(data)
+          const columns = valueColumns('slow_query.fields.')
+          return (
+            <CardTable
+              cardNoMargin
+              columns={columns}
+              items={items}
+              extendLastColumn
+            />
+          )
+        }
       }
-    },
-    {
-      key: 'time',
-      title: t('slow_query.detail.tabs.time'),
-      content: () => {
-        const items = tabTimeItems(data, t)
-        const columns = timeValueColumns('slow_query.fields.', items)
-        return (
-          <CardTable
-            cardNoMargin
-            columns={columns}
-            items={items}
-            extendLastColumn
-          />
-        )
-      }
-    },
-    {
-      key: 'copr',
-      title: t('slow_query.detail.tabs.copr'),
-      content: () => {
-        const items = tabCoprItems(data).filter((item) =>
-          columnsSet.has(item.key)
-        )
-        const columns = valueColumns('slow_query.fields.')
-        return (
-          <CardTable
-            cardNoMargin
-            columns={columns}
-            items={items}
-            extendLastColumn
-          />
-        )
-      }
-    },
-    {
-      key: 'txn',
-      title: t('slow_query.detail.tabs.txn'),
-      content: () => {
-        const items = tabTxnItems(data)
-        const columns = valueColumns('slow_query.fields.')
-        return (
-          <CardTable
-            cardNoMargin
-            columns={columns}
-            items={items}
-            extendLastColumn
-          />
-        )
-      }
+    ]
+    if (data.warnings) {
+      tbs.push({
+        key: 'warnings',
+        title: t('slow_query.detail.tabs.warnings'),
+        content: () => {
+          return (
+            <ReactJson
+              src={data.warnings! as any}
+              enableClipboard={false}
+              displayObjectSize={false}
+              displayDataTypes={false}
+              name={false}
+              iconStyle="circle"
+              groupArraysAfterLength={10}
+            />
+          )
+        }
+      })
     }
-  ]
+    return tbs
+  }, [schemaColumns, data, t])
   return <CardTabs animated={false} tabs={tabs} />
 }

d52938f35e391e8480f0b6277f3bd8064546a483
zzm <zhouzemin@pingcap.com>
2023-02-23 14:33:07 +0800

    Revert "upgrade etcd client (#1491)" (#1495)

--- util/client/pdclient/etcd_client.go
+++ util/client/pdclient/etcd_client.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/pingcap/log"
-	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/clientv3"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/backoff"

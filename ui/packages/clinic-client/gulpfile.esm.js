49f7f9ab84ea82b80649305f160537e469e34681
baurine <2008.hbl@gmail.com>
2023-05-09 17:51:02 +0800

    fix: fix skip gen api

--- ui/packages/clinic-client/gulpfile.esm.js
+++ ui/packages/clinic-client/gulpfile.esm.js
@@ -16,7 +16,7 @@ task('dev', series('swagger:gen', 'tsc:build'))
 
 // in netlify or vercel, we only build frontend, we don't need to generate api, else it will fail because we don't have go and java
 if (process.env.SKIP_GEN_API === '1') {
-  task('build', shell.task('echo "skip gen api"'))
+  task('build', shell.task('echo "skip gen api" & tsc'))
 } else {
   task('build', series('swagger:gen', 'tsc:build'))
 }

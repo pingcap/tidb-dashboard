a8d3011d2eae21f6b06cd08441ae7bbebc0ce368
baurine <2008.hbl@gmail.com>
2023-06-05 17:54:37 +0800

    chore: remove slow query v2 pages

--- ui/packages/tidb-dashboard-lib/src/apps/SlowQuery/index.tsx
+++ ui/packages/tidb-dashboard-lib/src/apps/SlowQuery/index.tsx
@@ -5,13 +5,12 @@ import useCache, { CacheContext } from '@lib/utils/useCache'
 
 import { addTranslations } from '@lib/utils/i18n'
 
-import { List, Detail, ListV2 } from './pages'
+import { List, Detail } from './pages'
 
 import { SlowQueryContext } from './context'
 
 import translations from './translations'
 import { useLocationChange } from '@lib/hooks/useLocationChange'
-import { SlowQueryComparison } from './pages/Comparison'
 
 addTranslations(translations)
 
@@ -22,12 +21,6 @@ function AppRoutes() {
     <Routes>
       <Route path="/slow_query" element={<List />} />
       <Route path="/slow_query/detail" element={<Detail historyBack />} />
-      <Route path="/slow_query/v2" element={<ListV2 />} />
-      <Route path="/slow_query/v2/detail" element={<Detail historyBack />} />
-      <Route
-        path="/slow_query/v2/comparison"
-        element={<SlowQueryComparison />}
-      />
     </Routes>
   )
 }

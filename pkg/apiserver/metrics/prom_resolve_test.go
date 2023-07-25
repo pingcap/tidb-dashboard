33372d3f4540abafe12706c8a23a4cc25889d2f7
mornyx <mornyx.z@gmail.com>
2023-07-25 10:36:01 +0800

    tweak: allows prom addr with http path
    Signed-off-by: mornyx <mornyx.z@gmail.com>

---
+++ pkg/apiserver/metrics/prom_resolve_test.go
@@ -0,0 +1,22 @@
+package metrics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// https://github.com/pingcap/tidb-dashboard/issues/1560
+func Test_normalizeCustomizedPromAddress(t *testing.T) {
+	addr, err := normalizeCustomizedPromAddress("http://infra-tidb-monitoring-shadow2-prod-0a01da41:9090")
+	require.NoError(t, err)
+	require.Equal(t, "http://infra-tidb-monitoring-shadow2-prod-0a01da41:9090", addr)
+
+	addr, err = normalizeCustomizedPromAddress("http://infra-tidb-monitoring-shadow2-prod-0a01da41:9090/")
+	require.NoError(t, err)
+	require.Equal(t, "http://infra-tidb-monitoring-shadow2-prod-0a01da41:9090", addr)
+
+	addr, err = normalizeCustomizedPromAddress("http://infra-tidb-monitoring-shadow2-prod-0a01da41:9090/_/tsdb/")
+	require.NoError(t, err)
+	require.Equal(t, "http://infra-tidb-monitoring-shadow2-prod-0a01da41:9090/_/tsdb", addr)
+}

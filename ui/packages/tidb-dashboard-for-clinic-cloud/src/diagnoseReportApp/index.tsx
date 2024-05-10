3ac0626384adf50f0d865923f81458a7a6c5b04f
Sparkle <1284531+baurine@users.noreply.github.com>
2024-05-10 17:23:14 +0800

    chore: hide some features for clinic and update distro res (#1674)

--- ui/packages/tidb-dashboard-for-clinic-cloud/src/diagnoseReportApp/index.tsx
+++ ui/packages/tidb-dashboard-for-clinic-cloud/src/diagnoseReportApp/index.tsx
@@ -10,7 +10,7 @@ import DiagnosisReport from './components/DiagnosisReport'
 import translations from './translations'
 
 // for update distro strings resource
-// import '~/utils/distro/stringsRes'
+import '~/utils/distro/stringsRes'
 
 import './index.css'
 

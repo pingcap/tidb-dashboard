c891b1928c1ca9bf88a0d7847acf99d7d224c6b4
lhy1024 <admin@liudos.us>
2024-07-30 20:09:12 +0800

    fix panic
    Signed-off-by: lhy1024 <admin@liudos.us>

--- pkg/keyvisual/region/utils.go
+++ pkg/keyvisual/region/utils.go
@@ -8,10 +8,16 @@ import (
 
 // String converts slice of bytes to string without copy.
 func String(b []byte) string {
+	if len(b) == 0 {
+		return ""
+	}
 	return unsafe.String(&b[0], len(b)) // #nosec
 }
 
 // Bytes converts a string into a byte slice. Need to make sure that the byte slice is not modified.
 func Bytes(s string) []byte {
+	if len(s) == 0 {
+		return nil
+	}
 	return unsafe.Slice(unsafe.StringData(s), len(s)) // #nosec
 }

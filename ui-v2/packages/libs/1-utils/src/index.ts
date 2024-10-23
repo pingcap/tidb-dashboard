313ca69610a80126192d03c2cc5d323329c61995
baurine <2008.hbl@gmail.com>
2024-10-23 21:57:14 +0800

    chore: init src

--- ui-v2/packages/libs/1-utils/src/index.ts
+++ ui-v2/packages/libs/1-utils/src/index.ts
@@ -1,19 +1,3 @@
-import { TOptions } from "i18next";
-import { useCallback, useMemo } from "react";
-import { useTranslation } from "react-i18next";
-
-export function useTn() {
-  const { t } = useTranslation();
-
-  const tn = useCallback(
-    (i18nKey: string, defVal: string, options?: TOptions) => {
-      return t(i18nKey, defVal, options);
-    },
-    [t],
-  );
-  const ret = useMemo(() => {
-    return { tn };
-  }, [tn]);
-
-  return ret;
-}
+export * from "./hooks";
+export * from "@tidbcloud/uikit/hooks";
+export * from "@tidbcloud/uikit/utils";

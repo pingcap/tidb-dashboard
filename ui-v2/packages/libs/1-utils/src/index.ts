91ef9e5ef440b811365bb92eb264cfe63e920037
baurine <2008.hbl@gmail.com>
2024-10-23 17:58:52 +0800

    chore: config utils

---
+++ ui-v2/packages/libs/1-utils/src/index.ts
@@ -0,0 +1,19 @@
+import { TOptions } from "i18next";
+import { useCallback, useMemo } from "react";
+import { useTranslation } from "react-i18next";
+
+export function useTn() {
+  const { t } = useTranslation();
+
+  const tn = useCallback(
+    (i18nKey: string, defVal: string, options?: TOptions) => {
+      return t(i18nKey, defVal, options);
+    },
+    [t],
+  );
+  const ret = useMemo(() => {
+    return { tn };
+  }, [tn]);
+
+  return ret;
+}

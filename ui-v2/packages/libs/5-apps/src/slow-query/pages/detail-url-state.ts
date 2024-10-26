487e15f9139d976789bc3c64cee7b716f1b527a1
baurine <2008.hbl@gmail.com>
2024-10-26 23:30:06 +0800

    chore: use @baurine/use-url-state lib

--- ui-v2/packages/libs/5-apps/src/slow-query/pages/detail-url-state.ts
+++ ui-v2/packages/libs/5-apps/src/slow-query/pages/detail-url-state.ts
@@ -1,4 +1,4 @@
-import { useUrlState } from "@pingcap-incubator/tidb-dashboard-lib-utils"
+import { useUrlState } from "@baurine/use-url-state"
 
 type DetailUrlState = Partial<Record<"id", string>>
 

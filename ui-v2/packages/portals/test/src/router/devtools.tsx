d9a33f4640170d5ebb9f261ce87d19f5bde5619a
baurine <2008.hbl@gmail.com>
2025-03-27 13:32:08 +0800

    chore: config tanstack router devtool

--- ui-v2/packages/portals/test/src/router/devtools.tsx
+++ ui-v2/packages/portals/test/src/router/devtools.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 
 export const RouterDevtools =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "production" ||
+  import.meta.env.VITE_TANSTACK_ROUTER_DEVTOOLS_ENABLED !== "true"
     ? () => null
     : React.lazy(() =>
         import("@tanstack/router-devtools").then((res) => ({

4fc763343cd2ffc40ffdd2db3434b5b4a60e4b12
baurine <2008.hbl@gmail.com>
2025-03-27 16:12:50 +0800

    chore: remove index-advisor app

--- ui-v2/packages/libs/4-apps/rollup.config.js
+++ ui-v2/packages/libs/4-apps/rollup.config.js
@@ -7,7 +7,6 @@ export default {
     "slow-query/index": "src/slow-query/index.ts",
     "statement/index": "src/statement/index.ts",
     "metric/index": "src/metric/index.ts",
-    "index-advisor/index": "src/index-advisor/index.ts",
     // _re-exports
     "_re-exports/utils": "src/_re-exports/utils.ts",
     "_re-exports/charts": "src/_re-exports/charts.ts",

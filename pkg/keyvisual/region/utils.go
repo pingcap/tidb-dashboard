a5eda56c8f379105d007b04dc04a12453e8361ab
baurine <2008.hbl@gmail.com>
2025-08-01 18:47:14 +0800

    Revert "update net, crypto, image version, update go version to 1.23 (#1801) (#1816)"
    This reverts commit 82f59839fac98833800c7c2782245a971d23fa11.

--- pkg/keyvisual/region/utils.go
+++ pkg/keyvisual/region/utils.go
@@ -12,8 +12,8 @@ func String(b []byte) (s string) {
 	if len(b) == 0 {
 		return ""
 	}
-	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))   //nolint:staticcheck
-	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s)) //nolint:staticcheck
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))   // #nosec
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s)) // #nosec
 	pstring.Data = pbytes.Data
 	pstring.Len = pbytes.Len
 	return
@@ -24,8 +24,8 @@ func Bytes(s string) (b []byte) {
 	if len(s) == 0 {
 		return
 	}
-	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))   //nolint:staticcheck
-	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s)) //nolint:staticcheck
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))   // #nosec
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s)) // #nosec
 	pbytes.Data = pstring.Data
 	pbytes.Len = pstring.Len
 	pbytes.Cap = pstring.Len

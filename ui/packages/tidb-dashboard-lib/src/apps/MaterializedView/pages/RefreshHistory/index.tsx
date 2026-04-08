d4f6e570082c4be29ac9abf65c5a6650276f1815
Suhaha <jklopsdfw@gmail.com>
2026-04-08 09:49:17 +0800

    fix: update applyFilters button click handler to use arrow function

--- ui/packages/tidb-dashboard-lib/src/apps/MaterializedView/pages/RefreshHistory/index.tsx
+++ ui/packages/tidb-dashboard-lib/src/apps/MaterializedView/pages/RefreshHistory/index.tsx
@@ -438,7 +438,7 @@ export default function RefreshHistory() {
               style={{ width: 180 }}
             />
 
-            <Button type="primary" onClick={applyFilters}>
+            <Button type="primary" onClick={() => applyFilters()}>
               {t('materialized_view.filters.query')}
             </Button>
           </Space>
